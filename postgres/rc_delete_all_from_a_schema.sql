bd38b2dad543f608272331318e2319451bfa7e77
Remi-C <Remi-C@fake.fake>
2013-12-03 17:19:03 +0100

    Changed the function to improve cosmetic added a new function to delete allt able froma  schema regardless of geometry

--- postgres/rc_delete_all_from_a_schema.sql
+++ postgres/rc_delete_all_from_a_schema.sql
@@ -1,44 +1,58 @@
-﻿/*
-Rémi Cura 
-THALES INTERNAL
-22/08/2012
-
-boolean delete_all_from_a_schema_to_another(schema_name text)
-This function delete all the geometric data tables from a schema.
-WARNING: always returns true, no control of execution,
-prototype : not properly tested and proofed.
-
-*/
-
-
-
-DROP FUNCTION IF EXISTS rc_delete_all_from_a_schema(text);--remove the function before re-creating it : act as a security versus function-type change
-
-CREATE OR REPLACE FUNCTION rc_delete_all_from_a_schema(schema_name text) RETURNS boolean
-AS $$
+﻿-----------------
+--Rémi Cura 
+--Thales/IGN
+--3/12/2013
+--
+--2 functions to delete all table from a schema
+--one to delete only geometry table
+--the other to delete all table.
+--WARNING : this is CASCADE ! 
+--don't use this in a schema with spatial_ref_sys or whole postgis will be removed
+--
+--
+DROP FUNCTION IF EXISTS public.rc_drop_all_geom_in_schema(text);--remove the function before re-creating it : act as a security versus function-type change
+CREATE OR REPLACE FUNCTION public.rc_drop_all_geom_in_schema( schema_name text) RETURNS boolean
+AS $$ 
 DECLARE
-    row record;
+    _r record;
     result boolean;
-    the_query text;
 BEGIN
-	FOR row IN /*all tables in geometry_columns wich are not postgis specific table and are in the old schema*/
-	SELECT *
-	FROM geometry_columns
-	WHERE f_table_schema = schema_name
-		AND f_table_name <> 'raster_columns'--remove postgis table of the selection
-
-	LOOP--loop on all tbale in schema
-		BEGIN
-		the_query := ' DROP TABLE '|| schema_name ||'.' || row.f_table_name || '; ' ;
-		
-		EXECUTE the_query ;
-		END;
-
-	END LOOP;
+	FOR _r IN /*all tables in geometry_columns wich are not postgis specific table and are in the old schema*/
+		SELECT *
+		FROM geometry_columns
+		WHERE f_table_schema = schema_name
+			AND f_table_name <> 'raster_columns'--remove postgis table of the selection
+    LOOP
+		EXECUTE  format(' DROP TABLE IF EXISTS %I.%I CASCADE;', _schema_name,_r.tablename ) ;
+    END LOOP;
 	RETURN TRUE;
 END;
 $$LANGUAGE plpgsql; 
 
 /*exemple use-case :*/
---SELECT rc_delete_all_from_a_schema('odparis_reworked'::Text);
+--SELECT rc_delete_all_geom_from_a_schema('odparis_reworked'::Text);
 
+
+DROP FUNCTION IF EXISTS public.rc_drop_all_in_schema(text);--remove the function before re-creating it : act as a security versus function-type change
+
+CREATE OR REPLACE FUNCTION public.rc_drop_all_in_schema(_schema_name text) RETURNS boolean
+AS $$
+DECLARE
+    _r record;
+    result boolean;
+BEGIN
+	FOR _r IN /*all tables in geometry_columns wich are not postgis specific table and are in the old schema*/
+	SELECT *
+	FROM pg_tables
+	WHERE schemaname = _schema_name
+		 
+
+    LOOP
+		EXECUTE  format(' DROP TABLE IF EXISTS %I.%I CASCADE;', _schema_name,_r.tablename ) ;
+    END LOOP;
+	RETURN TRUE;
+END;
+$$LANGUAGE plpgsql; 
+
+/*exemple use-case :*/
+--SELECT rc_drop_all_in_schema('demo_zone_test'::Text);
