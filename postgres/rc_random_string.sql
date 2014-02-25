78eff39f27051118768d249fccda0b82a4ad03c4
Remi-C <Remi-C@fake.fake>
2014-02-25 12:22:43 +0100

    new function to create random string

---
+++ postgres/rc_random_string.sql
@@ -0,0 +1,13 @@
+﻿CREATE OR REPLACE FUNCTION rc_random_string(INTEGER )
+RETURNS text AS $$
+	SELECT array_to_string(
+		ARRAY(
+			SELECT 
+				substring('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789' FROM (random()*36)::int + 1 FOR 1) 
+				FROM generate_series(1,$1)
+		)
+	,'')
+
+	--exemple use case :
+	--SELECT rc_random_string(10);
+$$ LANGUAGE sql;
