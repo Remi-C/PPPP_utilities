93cfeebd8e4300850f55add4c97bb3882a1f638b
Remi-C <Remi-C@fake.fake>
2014-03-06 14:50:25 +0100

    added a unnest function with ordering, from internet

---
+++ postgres/rc_unnest_with_ordinality.sql
@@ -0,0 +1,18 @@
+﻿
+
+CREATE OR REPLACE FUNCTION unnest_with_ordinality(anyarray, OUT value
+anyelement, OUT ordinality integer)
+  RETURNS SETOF record AS
+$$
+--@param an array to unnest
+--@return : a set of [element,element ordinalty]
+
+
+--note : found on internet 
+--this function allow to reliabily use the unnest to always unnest in the same order 
+
+SELECT $1[i] AS ordinality, i AS array FROM
+    generate_series(array_lower($1,1),
+                    array_upper($1,1)) i;
+$$
+LANGUAGE sql IMMUTABLE; 
