3bab7eb56d2a57b97899695421dda21952ff7b1f
Remi-C <Remi-C@fake.fake>
2014-03-28 21:02:55 +0100

    dding an internet function to aggregate array

---
+++ postgres/array_of_array.sql
@@ -0,0 +1,12 @@
+﻿----------------
+--Found on the net (no copyright)
+--
+--
+
+--this function allows to create array of array.
+--to use it, call it on array data, it will output array of array
+CREATE AGGREGATE array_agg_custom(anyarray)
+(
+    SFUNC = array_cat,
+    STYPE = anyarray
+);
