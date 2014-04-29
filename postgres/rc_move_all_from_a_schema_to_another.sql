b638ec9d5bf961bff1594150266c33b82666df7f
Remi-C <Remi-C@fake.fake>
2014-04-29 10:26:40 +0200

    changin the move_all to move all(not only geom table) few typo and doc

--- postgres/rc_move_all_from_a_schema_to_another.sql
+++ postgres/rc_move_all_from_a_schema_to_another.sql
@@ -1,37 +1,30 @@
-﻿/*
-Rémi Cura 
-THALES INTERNAL
-22/08/2012
+﻿---------------------------------------------
+--Rémi-C , THALES
+--22/08/2012
+--
+--Moving all table from a schema to another
+-----------------------------------------------
 
-boolean move_all_from_a_schema_to_another(old_schema text, new_schema text)
-This function moves all the geometric data tables from on schema over an other.
-WARNING: always returns true, no control of execution,
-prototype : not properly tested and proofed.
+--WARNING: always returns true, no control of execution, prototype : not properly tested and proofed.
 
+ 
+ 
 
-DEPENDS ON : 
-boolean move_table_from_a_schema_to_another(old_schema text, new_schema text, table_name text) 
-This function moves a table from a schema to another
-same WARNING applies.
-
-*/
-
-
-
-DROP FUNCTION rc_move_all_from_a_schema_to_another(text,text);--remove the function before re-creating it : act as a security versus function-type change
-
+DROP FUNCTION IF EXISTS rc_move_all_from_a_schema_to_another(text,text);--remove the function before re-creating it : act as a security versus function-type change
 CREATE OR REPLACE FUNCTION rc_move_all_from_a_schema_to_another(old_schema text, new_schema text) RETURNS boolean
 AS $$
+	--@brief : this function move all the table from a given schema to another.
+	--@param : the old schema
+	--@param : the new schema
+	--@return : return always true
+
 DECLARE
     row record;
     result boolean;
 BEGIN
 	FOR row IN /*all tables in geometry_columns wich are not postgis specific table and are in the old schema*/
-	SELECT *
-	FROM geometry_columns
-	WHERE f_table_schema = old_schema
-		AND f_table_name <> 'raster_columns'--remove postgis table
-
+	SELECT table_name FROM information_schema.tables 
+	WHERE table_schema = old_schema  
     LOOP
 		result := rc_move_table_from_a_schema_to_another( old_schema, new_schema, row.f_table_name );
     END LOOP;
@@ -46,10 +39,16 @@ $$LANGUAGE plpgsql;
 
 
 
-DROP FUNCTION rc_move_table_from_a_schema_to_another(text,text,text);
+DROP FUNCTION IF EXISTS  rc_move_table_from_a_schema_to_another(text,text,text);
 
 CREATE OR REPLACE FUNCTION rc_move_table_from_a_schema_to_another(old_schema text, new_schema text, table_name text) RETURNS boolean
 AS $$
+	--@brief : this function move one table from a given schema to another. The table must __not__ be schema qualified. 
+	--@param : the old schema
+	--@param : the new schema
+	--@param : the table name (no schema qualifier)
+	--@return : return always true
+	
 DECLARE
 BEGIN
         EXECUTE 'ALTER TABLE ' || quote_ident(old_schema) || '.' || quote_ident(table_name) || ' SET SCHEMA ' || quote_ident(new_schema) || ';' ;
