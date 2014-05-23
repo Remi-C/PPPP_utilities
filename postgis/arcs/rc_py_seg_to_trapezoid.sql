868587ad30154d763d1e7be0867c34aa40c899f8
Remi-C <Remi-C@fake.fake>
2014-05-23 12:39:13 +0200

    support function ot perform a variable radius buffer per summit

---
+++ postgis/arcs/rc_py_seg_to_trapezoid.sql
@@ -0,0 +1,75 @@
+﻿-------------------------------
+-- Remi-C , Thales IGN, 2014
+--
+--
+--
+--support function : given a segment and 2 floats, compute the trapesoid formed by the perpendicular to the segment passing by 2 end point and of length of the float
+--	 tis python function demonstrate how to read and write geometry from postgis to shapely to postgis, and perform some computing using numpy
+------------------------------
+
+
+ --------------------- python function to compute the center of circle given 2 segments and Radius OR tangency point
+ 
+DROP FUNCTION IF EXISTS rc_py_seg_to_trapezoid ( igeom bytea,r1 float ,r2 FLOAT );
+CREATE OR REPLACE FUNCTION rc_py_seg_to_trapezoid ( igeom bytea,r1 float,r2 FLOAT
+, OUT o_geo geometry )  
+AS $$
+	###
+	#this function assume that the input geom is a segment
+
+		
+	##import of packages
+	#importing the numpy package to be able ot perform vector operation
+	import numpy as np; 
+
+	#importing the shapely package to perform geometry manipulation
+	from shapely import wkb ; #loading geometry from postgres
+	from shapely.geometry import asMultiPoint #to cast point to numpy array
+	from shapely.geometry import asPolygon #to cast array to polygon
+	#pretty print :
+	import pprint as pp;
+	pp1 = pp.PrettyPrinter(indent=4,depth=6,width=50) ;
+	  
+	##importing the geom
+
+	#importing the geometry #NOTE : if outside postgres, hex = True, If inside postgres, Hex = False
+	geom = wkb.loads( igeom, hex=False ) ;
+
+	p = np.asarray(geom) ;#putting the geom into an array
+
+
+
+	 #compute normal orientation of the segment
+	normal =  (p[1]-p[0]) * np.array([ 1,-1] ) ; 
+	normal[0], normal[1], = normal[1],normal[0] ; #exchanging x and y, wihtout copy
+	normal = normal/np.linalg.norm(normal) ;
+	print(normal) ;
+	 
+	 #creating the upper point and down point for first and second (hopefully last) point in segment
+	p1u = p[0] + normal * r1 ;
+	p1d = p[0] - normal * r1 ;
+
+	p2u = p[1] + normal * r2 ;
+	p2d = p[1] - normal * r2 ;
+	 
+	output_line = (p1u,p2u,p2d,p1d,p1u) ;
+	 
+	ogeom = asPolygon(output_line) ;
+	 
+
+	print(ogeom) ; 
+	pp1.pprint( str(geom)); 
+
+	#outputing for postgis : @NOTE : if inside postgres, hex =False, if outside, hex=True
+	output = wkb.dumps(ogeom, hex=True);
+
+	 
+	return   output   ;
+	#return { "center": center, "radius": radius ,  "t1": t1, "t2":t2} 
+$$ LANGUAGE plpythonu;
+
+SELECT * ,  St_Astext(rc_py_seg_to_trapezoid( geom   ,2 ::float,3::float )   )
+FROM ST_GeomFromtext('LINESTRING(0 0 , 10 0 )') AS geom;
+
+
+
---
+++ postgis/arcs/rc_variableBuffer.sql
@@ -0,0 +1,79 @@
+﻿-------------------------------
+-- Remi-C , Thales IGN, 2014
+--
+--
+--
+--variable radius buffer
+------------------------------
+
+
+DROP FUNCTION IF EXISTS public.rc_variableBuffer(p1 geometry, radius float[]);
+		CREATE FUNCTION public.rc_variableBuffer(p1 geometry, radius float[])
+			RETURNS geometry AS
+		$BODY$
+			--@brief this function computes a variable buffer given a geometry and an array of buffer raidus to b e applied on each point of the geom
+			--@WARNING : very naive implementation.
+			--Idea from Mathieu B.
+			DECLARE 
+			result geometry := NULL;
+			 segs geometry; 
+			BEGIN
+
+				--breaking geometry into segments.
+				
+			return NULL;
+			END ;
+		$BODY$
+		LANGUAGE plpgsql IMMUTABLE;
+
+	---testing
+	SELECT rc_variableBuffer(geom,  ARRAY[1,2,3])
+	FROM ST_GeomFromtext('LINESTRING(0 0 , 10 0 , 20 20)') AS geom;
+
+
+	with the_geom AS (
+	SELECT ST_GeomFromtext('LINESTRING(0 0 , 10 0 , 20 20, 20 5, 5 10 )') AS geom, ARRAY[1,2,3,4,5] AS radiuses
+	)
+	,dump AS (
+		SELECT DISTINCT radiuses,row_number() over() AS id,  dmp.*
+		FROM the_geom as g, rc_DumpSegments(geom ) AS dmp
+	)
+	,trapezoid AS (
+	SELECT rc_py_seg_to_trapezoid(geom, radiuses[id],radiuses[id+1]) AS geom
+	FROM dump
+	)
+	,pts_and_radius AS (
+	SELECT (ST_DumpPoints(geom)).geom, unnest(radiuses) AS radius
+	FROM the_geom
+	)
+	,buf_pts AS (
+	SELECT ST_Buffer(geom,radius) AS geom
+	FROM pts_and_radius
+	)
+	,all_geom AS (
+	SELECT geom
+	FROM buf_pts
+	UNION ALL 
+	SELECT geom
+	FROM trapezoid
+	)
+	,unioned_geom AS (
+	SELECT ST_union(geom) AS geom
+	FROM all_geom
+	)
+	,result AS (
+	
+		SELECT ST_Difference(tg.geom, ug.geom) AS geom --case whendoing a erosion
+		--SELECT ST_Union(tg.geom, ug.geom) AS geom --case whendoing a dilatation
+		FROM the_geom AS tg, unioned_geom AS ug
+	)
+	SELECT ST_Astext(geom)
+	FROM result 
+
+
+	 
+	SELECT St_AsText(ST_Union(geom, ST_Translate(geom, 5 ,-6 )))
+	FROM ST_GeomFromtext('LINESTRING M (0 0 1, 10 0 2, 20 20 3, 20 5 4, 5 10 5)') AS geom 
+
+ 
+ 
