7b9fe8fa650c303f72121c154b75ace547ad42f1
Remi-C <Remi-C@fake.fake>
2014-05-23 18:30:54 +0200

    corrected 2 bugs in max_circle : wrong formula to compute center (wrong direction), and forget a factor 2 in distance computation regarding the center

--- postgis/arcs/rc_py_compute_circle_from.sql
+++ postgis/arcs/rc_py_compute_circle_from.sql
@@ -6,7 +6,84 @@
 --support function : given 3 segments sharing an end point, and either a radius or the tangency point, compute the parameter of the circle that is tangent to both segment and
 --	passes by tangency point OR has given radius
 --	python function to compute the center of circle given 2 segments and Radius OR tangency point 
+--also a function that given 3 points (central, left, right), compute the biggest arc of circle possible with en d of arc being tangent to to segments
 ------------------------------ 
+
+
+
+DROP FUNCTION IF EXISTS rc_py_compute_max_circle ( f geometry(point), e geometry(point),g geometry(point));
+CREATE OR REPLACE FUNCTION rc_py_compute_max_circle ( i_f geometry(point), i_e geometry(point), i_g geometry(point)
+	, OUT center geometry(point),OUT  radius FLOAT,OUT  t1 geometry(point), OUT  t2 geometry(point))  
+AS $$
+in_server = True;
+	#importing the numpy package
+import numpy as np;
+    #importing shapely to read/write to from geom
+from shapely import wkb ; 
+from shapely.geometry import asPoint ;
+
+	#storing the point coordinates as vector to allow fast operations on it.
+_f = np.asarray( wkb.loads( i_f , hex=  in_server ) )  ;
+_e = np.asarray( wkb.loads( i_e , hex= in_server ) )  ;
+_g = np.asarray( wkb.loads( i_g , hex= in_server ) )  ;
+#_t1 = np.asarray( wkb.loads( i_t1 , hex= in_server ) )  ;
+
+_ef = _f-_e;
+_eg = _g-_e;
+
+#the max tangent circle is going to pass by the closest end of segment
+if np.linalg.norm(_ef) > np.linalg.norm(_eg) :
+	_t1 = _g;
+	plpy.notice('case when t1 is   G');
+else :
+	_t1 = _f;
+	plpy.notice('case when t1 is   F');
+	
+	# finding the _radius : = r = tan(theta) * dist(ET1), where theta is half of the angle beteen both segment 
+theta = np.arccos( np.dot(_ef,_eg)/(np.linalg.norm(_ef)*np.linalg.norm(_eg)) )/2 ;
+plpy.notice('theta',theta);
+	#computing the _radius of the circle
+_radius = np.tan(theta)*np.linalg.norm(_t1-_e) ;
+plpy.notice('radius',_radius) ; 
+#plpy.notice(theta) ; 
+	
+
+
+
+if np.array_equal(_t1,_g ) :
+	t2__ = _e + ((_ef)/np.linalg.norm(_ef)) * np.linalg.norm(_t1-_e) ;
+else :
+	t2__ = _e + ((_eg)/np.linalg.norm(_eg)) * np.linalg.norm(_t1-_e) ;
+
+#computing the position of the _center
+#getting the direction then moving of the right distance onto it
+# E + ( EF + EG )/(norm(EF+EG)) * norm(ET1)/cos(theta)
+_center =  _e +  ((_t1+t2__)/np.linalg.norm(_t1+t2__)  )* np.linalg.norm(_t1-_e) / (2*(np.dot(_ef,_eg)/(np.linalg.norm(_ef)*np.linalg.norm(_eg)) ));
+#_center = _e + (_ef+_eg) / (np.linalg.norm(_ef+_eg) ) * np.linalg.norm(_t1-_e) / (2*(np.dot(_ef,_eg)/(np.linalg.norm(_ef)*np.linalg.norm(_eg)) ));
+plpy.notice('center',_center) ;  
+
+center = wkb.dumps(asPoint(_center), hex=in_server) ;
+radius = _radius ;
+t1 =wkb.dumps(asPoint(_t1), hex=in_server) ;
+t2 = wkb.dumps(asPoint(t2__), hex=in_server) ;
+    
+    
+#plpy.notice(t2) ; 
+plpy.notice(_center  , radius  , _t1  , t2__ ) ;
+if in_server != True :  
+	print( center  , radius  , t1  , t2)
+else :
+	return [center,radius, t1,t2 ];
+	#return { "_center": _center, "_radius": _radius ,  "t1": t1, "t2":t2} 
+$$ LANGUAGE plpythonu;
+
+
+SELECT ST_Astext(center), radius, ST_Astext(t1),ST_Astext(t2)
+FROM rc_py_compute_max_circle(
+		ST_MakePoint(13,3)
+		,ST_MakePoint(0,0)
+		,ST_MakePoint(14,-4)
+		)  ; 
 
 
  
