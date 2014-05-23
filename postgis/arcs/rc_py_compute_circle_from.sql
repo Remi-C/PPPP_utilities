2ecc7768aed24b4d22464c4831d4693fae182729
Remi-C <Remi-C@fake.fake>
2014-05-23 16:20:12 +0200

    passed to geom output/input rather than float[]

--- postgis/arcs/rc_py_compute_circle_from.sql
+++ postgis/arcs/rc_py_compute_circle_from.sql
@@ -5,125 +5,138 @@
 --
 --support function : given 3 segments sharing an end point, and either a radius or the tangency point, compute the parameter of the circle that is tangent to both segment and
 --	passes by tangency point OR has given radius
---	 
-------------------------------
+--	python function to compute the center of circle given 2 segments and Radius OR tangency point 
+------------------------------ 
 
 
- --------------------- python function to compute the center of circle given 2 segments and Radius OR tangency point
-DROP FUNCTION IF EXISTS rc_py_compute_circle_from_tangency ( f float[2], e float[2],g float[2],t1 FLOAT[2]);
-CREATE OR REPLACE FUNCTION rc_py_compute_circle_from_tangency ( f float[2],e float[2],g float[2],t1 FLOAT[2] 
-, OUT center FLOAT[2],OUT  radius FLOAT,OUT  t1 FLOAT[2],OUT  t2 FLOAT[2])  
+ 
+DROP FUNCTION IF EXISTS rc_py_compute_circle_from_tangency ( f geometry(point), e geometry(point),g geometry(point),t1 geometry(point));
+CREATE OR REPLACE FUNCTION rc_py_compute_circle_from_tangency ( i_f geometry(point), i_e geometry(point), i_g geometry(point), i_t1 geometry(point)
+, OUT center geometry(point),OUT  radius FLOAT,OUT  t1 geometry(point), OUT  t2 geometry(point))  
 AS $$
-	###
-	#this function assume that there is 2 input segments with a common point being e (seg fe and eg), along with th point where the circle is tangent that is t1
-
-	
+in_server = True;
 	#importing the numpy package
-	import numpy as np;
+import numpy as np;
+    #importing shapely to read/write to from geom
+from shapely import wkb ; 
+from shapely.geometry import asPoint ;
+
 	#storing the point coordinates as vector to allow fast operations on it.
-	_f = np.asarray( f ) ;
-	_e = np.asarray( e ) ;
-	_g = np.asarray( g ) ;
-	_t1 = np.asarray( t1 ) ;
+_f = np.asarray( wkb.loads( i_f , hex=  in_server ) )  ;
+_e = np.asarray( wkb.loads( i_e , hex= in_server ) )  ;
+_g = np.asarray( wkb.loads( i_g , hex= in_server ) )  ;
+_t1 = np.asarray( wkb.loads( i_t1 , hex= in_server ) )  ;
 
-	_ef = _f-_e;
-	_eg = _g-_e;
+_ef = _f-_e;
+_eg = _g-_e;
 	
-	#@DEBUG finding the radius : = r = tan(theta) * dist(ET1), where theta is half of the angle beteen both segment
+	#@DEBUG finding the _radius : = r = tan(theta) * dist(ET1), where theta is half of the angle beteen both segment
 	##note that it is not necessary to explicit theta, we only need cos(theta), but we compute it for debug
-	theta = np.arccos( np.dot(_ef,_eg)/(np.linalg.norm(_ef)*np.linalg.norm(_eg)) )/2 ;
+theta = np.arccos( np.dot(_ef,_eg)/(np.linalg.norm(_ef)*np.linalg.norm(_eg)) )/2 ;
 
-	#computing the radius of the circle
-	radius = np.tan(theta)*np.linalg.norm(_t1-_e) ;
-	#plpy.notice(radius) ; 
-	#plpy.notice(theta) ; 
+	#computing the _radius of the circle
+_radius = np.tan(theta)*np.linalg.norm(_t1-_e) ;
+plpy.notice(_radius) ; 
+plpy.notice(theta) ; 
 	
-	#computing the position of the center
+	#computing the position of the _center
 	# E + ( EF + EG )/(norm(EF+EG)) * norm(ET1)/cos(theta)
-	center = _e + (_ef+_eg) / (np.linalg.norm(_ef+_eg) ) * np.linalg.norm(_t1-_e) / (np.dot(_ef,_eg)/(np.linalg.norm(_ef)*np.linalg.norm(_eg)) );
-	#plpy.notice(center) ;  
+_center = _e + (_ef+_eg) / (np.linalg.norm(_ef+_eg) ) * np.linalg.norm(_t1-_e) / (np.dot(_ef,_eg)/(np.linalg.norm(_ef)*np.linalg.norm(_eg)) );
+	#plpy.notice(_center) ;  
 	
-	t2_g = _e + ((_eg)/np.linalg.norm(_eg)) * np.linalg.norm(_t1-_e) ;
-	t2_f = _e + ((_ef)/np.linalg.norm(_ef)) * np.linalg.norm(_t1-_e) ;
+t2_g = _e + ((_eg)/np.linalg.norm(_eg)) * np.linalg.norm(_t1-_e) ;
+t2_f = _e + ((_ef)/np.linalg.norm(_ef)) * np.linalg.norm(_t1-_e) ;
 
-	if np.linalg.norm(t2_g - t1) < np.linalg.norm(t2_f - _t1) :
-		t2 = t2_f  ;
-	else :
-		t2 = t2_g  ;
+if np.linalg.norm(t2_g - _t1) < np.linalg.norm(t2_f - _t1) :
+	t2__ = t2_f  ;
+else :
+	t2__ = t2_g  ;
 
-	_t1 ;
-	#end if on side of t1
-
+center = wkb.dumps(asPoint(_center), hex=in_server) ;
+radius = _radius ;
+t1 = i_t1 ;
+t2 = wkb.dumps(asPoint(t2__), hex=in_server) ;
+    
+    
+#plpy.notice(t2) ; 
+if in_server != True :  
+	print( center  , radius  , t1  , t2)
+else :
+	return [center,radius, t1,t2 ];
+	#return { "_center": _center, "_radius": _radius ,  "t1": t1, "t2":t2}
 	
-	plpy.notice(t2) ; 
-	return  [center  , radius  , _t1  , t2]   ;
-	#return { "center": center, "radius": radius ,  "t1": t1, "t2":t2}
-	
+
 $$ LANGUAGE plpythonu;
 
+--testing
 SELECT *
 FROM rc_py_compute_circle_from_tangency(
-		ARRAY[13,3]
-		,ARRAY[0,0]
-		,ARRAY[14,-4]
-		,ARRAY[6,-2]
-		)  ;
+		ST_MakePoint(13,3)
+		,ST_MakePoint(0,0)
+		,ST_MakePoint(14,-4)
+		,ST_MakePoint(6,-2)
+		)  ; 
 
 
 
-
-
-DROP FUNCTION IF EXISTS rc_py_compute_circle_from_radius ( f float[2], e float[2],g float[2],r FLOAT );
-CREATE OR REPLACE FUNCTION rc_py_compute_circle_from_radius ( f float[2],e float[2],g float[2],r FLOAT  
-, OUT center FLOAT[2],OUT  radius FLOAT,OUT  t1 FLOAT[2],OUT  t2 FLOAT[2])  
+DROP FUNCTION IF EXISTS rc_py_compute_circle_from_radius ( f geometry(point), e geometry(point),g geometry(point),r FLOAT );
+CREATE OR REPLACE FUNCTION rc_py_compute_circle_from_radius ( i_f geometry(point), i_e geometry(point), i_g geometry(point), i_r FLOAT  
+, OUT center geometry(point),OUT  radius FLOAT,OUT  t1 geometry(point),OUT  t2 geometry(point))  
 AS $$
 	###
 	#this function assume that there is 2 input segments with a common point being e (seg fe and eg), along with radius of the circle that is tangent ot both seg
-
-	
+ 
+in_server = True;
 	#importing the numpy package
-	import numpy as np;
-	#storing the point coordinates as vector to allow fast operations on it.
-	_f = np.asarray( f ) ;
-	_e = np.asarray( e ) ;
-	_g = np.asarray( g ) ;
-	radius = r;
+import numpy as np;
+    #importing shapely to read/write to from geom
+from shapely import wkb ;
+from shapely.geometry import asPoint ;
 
-	_ef = _f-_e;
-	_eg = _g-_e;
-	
-	#@DEBUG finding the radius : = r = tan(theta) * dist(ET1), where theta is half of the angle beteen both segment
-	##note that it is not necessary to explicit theta, we only need cos(theta), but we compute it for debug
-	theta = np.arccos( np.dot(_ef,_eg)/(np.linalg.norm(_ef)*np.linalg.norm(_eg)) )/2 ;
+	#storing the point coordinates as vector to allow fast operations on it.
+_f = np.asarray( wkb.loads( i_f , hex=  in_server ) )  ;
+_e = np.asarray( wkb.loads( i_e , hex= in_server ) )  ;
+_g = np.asarray( wkb.loads( i_g , hex= in_server ) )  ;
+
+_ef = _f-_e;
+_eg = _g-_e;
+
+	#@DEBUG finding the radius : = r = tan(theta) * dist(ET1), where theta is half of the angle beteen both segment 
+theta = np.arccos( np.dot(_ef,_eg)/(np.linalg.norm(_ef)*np.linalg.norm(_eg)) )/2 ;
 
 	#computing the point T1, on EF
-	_t1 = _e + (_ef / np.linalg.norm(_ef) )* radius / np.tan(theta) ; 
+_t1 = _e + (_ef / np.linalg.norm(_ef) )* i_r/ np.tan(theta) ; 
 	#plpy.notice(radius) ; 
 	#plpy.notice(theta) ; 
 	
 	#computing the position of the center
 	# E + ( EF + EG )/(norm(EF+EG)) * norm(ET1)/cos(theta)
-	center = _e + (_ef+_eg) / (np.linalg.norm(_ef+_eg) ) * np.linalg.norm(_t1-_e) / (np.dot(_ef,_eg)/(np.linalg.norm(_ef)*np.linalg.norm(_eg)) );
+_center = _e + (_ef+_eg) / (np.linalg.norm(_ef+_eg) ) * np.linalg.norm(_t1-_e) / (np.dot(_ef,_eg)/(np.linalg.norm(_ef)*np.linalg.norm(_eg)) );
 	#plpy.notice(center) ;  
 	
-	t2  = _e + ((_eg)/np.linalg.norm(_eg)) * np.linalg.norm(_t1-_e) ;
-	t1 = _t1 ;
-	plpy.notice(t2) ; 
-	return  [center  , radius  , t1  , t2]   ;
-	return { "center": center, "radius": radius ,  "t1": t1, "t2":t2}
-	
+_t2  = _e + ((_eg)/np.linalg.norm(_eg)) * np.linalg.norm(_t1-_e) ; 
+#	plpy.notice(t2) ;
+
+
+center = wkb.dumps(asPoint(_center), hex=in_server) ;
+radius = i_r  ;
+t1 = wkb.dumps(asPoint(_t1), hex=in_server) ;
+t2 = wkb.dumps(asPoint(_t2), hex=in_server) ;
+
+
+return  [ center, radius  , t1 , t2 ]   ;	
 $$ LANGUAGE plpythonu;
 
+--testing
 SELECT *
 FROM rc_py_compute_circle_from_radius(
-		ARRAY[13,3]
-		,ARRAY[0,0]
-		,ARRAY[14,-4]
+		ST_MakePoint(13,3)
+		,ST_MakePoint(0,0)
+		,ST_MakePoint(14,-4)
 		,1.63
 		)  ;
 
 
-		
 /*
 DROP FUNCTION IF EXISTS rc_py_distance_point_line ( a float[2], b float[2],p float[2] );
 CREATE OR REPLACE FUNCTION rc_py_distance_point_line ( a float[2], b float[2],p float[2])
