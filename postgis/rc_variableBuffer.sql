d521d41cbdecffca764e6bff36babe4b80d2e08e
Remi C <remi.fake@fake.com>
2014-07-18 16:49:23 +0200

    working function. Now radius is given BY M dimension rahter than explicitly

--- postgis/rc_variableBuffer.sql
+++ postgis/rc_variableBuffer.sql
@@ -7,30 +7,60 @@
 ------------------------------
 
 
-DROP FUNCTION IF EXISTS public.rc_variableBuffer(p1 geometry, radius float[]);
-		CREATE FUNCTION public.rc_variableBuffer(p1 geometry, radius float[])
+DROP FUNCTION IF EXISTS public.rc_variableBuffer(i_geom geometry );
+		CREATE FUNCTION public.rc_variableBuffer(i_geom geometry , OUT buffered_geom geometry)
 			RETURNS geometry AS
 		$BODY$
-			--@brief this function computes a variable buffer given a geometry and an array of buffer raidus to b e applied on each point of the geom
+			--@brief this function computes a variable buffer given a geometry and a raidus in M dimension of each point
 			--@WARNING : very naive implementation.
 			--Idea from Mathieu B.
-			DECLARE 
-			result geometry := NULL;
+			DECLARE  
 			 segs geometry; 
 			BEGIN
-
-				--breaking geometry into segments.
 				
-			return NULL;
+				with dump AS (
+					SELECT DISTINCT dmp.*
+					FROM   rc_DumpSegments(i_geom ) AS dmp
+				)
+				,trapezoid AS (
+				SELECT rc_py_seg_to_trapezoid(ST_Force2D(geom), ST_M(ST_StartPoint(geom)),ST_M(ST_EndPoint(geom))) AS geom
+				FROM dump
+				)
+				,pts_and_radius AS (
+				SELECT (ST_DumpPoints(i_geom)).geom  
+				)
+				,buf_pts AS (
+				SELECT ST_Buffer(geom,ST_M(geom)) AS geom
+				FROM pts_and_radius
+				)
+				,all_geom AS (
+				SELECT geom
+				FROM buf_pts
+				UNION ALL 
+				SELECT geom
+				FROM trapezoid
+				)
+				,unioned_geom AS (
+				SELECT ST_union(geom) AS geom
+				FROM all_geom
+				)
+				--,result AS (
+				
+					--SELECT ST_Difference(tg.geom, ug.geom) AS geom --case whendoing a erosion
+				--	SELECT ST_Union(tg.geom, ug.geom) AS geom --case whendoing a dilatation
+				--FROM result ;
+				SELECT geom INTO buffered_geom
+				FROM unioned_geom 
+			return  ;
 			END ;
 		$BODY$
 		LANGUAGE plpgsql IMMUTABLE;
 
 	---testing
-	SELECT rc_variableBuffer(geom,  ARRAY[1,2,3])
-	FROM ST_GeomFromtext('LINESTRING(0 0 , 10 0 , 20 20)') AS geom;
+	SELECT ST_AsText(rc_variableBuffer(geom ))
+	FROM ST_GeomFromtext('LINESTRINGM(0 0 1, 10 0 2, 20 20 3 )') AS geom;
 
-
+/*
 	with the_geom AS (
 	SELECT ST_GeomFromtext('LINESTRING(0 0 , 10 0 , 20 20, 20 5, 5 10 )') AS geom, ARRAY[1,2,3,4,5] AS radiuses
 	)
@@ -63,17 +93,17 @@ DROP FUNCTION IF EXISTS public.rc_variableBuffer(p1 geometry, radius float[]);
 	)
 	,result AS (
 	
-		SELECT ST_Difference(tg.geom, ug.geom) AS geom --case whendoing a erosion
-		--SELECT ST_Union(tg.geom, ug.geom) AS geom --case whendoing a dilatation
+		--SELECT ST_Difference(tg.geom, ug.geom) AS geom --case whendoing a erosion
+		SELECT ST_Union(tg.geom, ug.geom) AS geom --case whendoing a dilatation
 		FROM the_geom AS tg, unioned_geom AS ug
 	)
 	SELECT ST_Astext(geom)
-	FROM result 
+	FROM result ;
 
 
 	 
-	SELECT St_AsText(ST_Union(geom, ST_Translate(geom, 5 ,-6 )))
-	FROM ST_GeomFromtext('LINESTRING M (0 0 1, 10 0 2, 20 20 3, 20 5 4, 5 10 5)') AS geom 
+	--SELECT St_AsText(ST_Union(geom, ST_Translate(geom, 5 ,-6 )))
+	--FROM ST_GeomFromtext('LINESTRING M (0 0 1, 10 0 2, 20 20 3, 20 5 4, 5 10 5)') AS geom ;
 
- 
+ */
  
