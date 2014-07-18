b31c6b69adb0df0683816ef99848f39f31d5064e
Remi C <remi.fake@fake.com>
2014-07-18 16:15:33 +0200

    new function found on postgres wiki

---
+++ postgres/array_reverse.sql
@@ -0,0 +1,15 @@
+﻿-------------------------------------
+-- Remi-C 
+--this function was found on the postgresql wiki !
+
+
+ CREATE OR REPLACE FUNCTION array_reverse(anyarray) RETURNS anyarray AS $$ --@brief : return the array in the reverse order
+SELECT ARRAY(
+    SELECT $1[i]
+    FROM generate_subscripts($1,1) AS s(i)
+    ORDER BY i DESC
+);
+$$ LANGUAGE 'sql' STRICT IMMUTABLE;
+
+--test : 
+SELECT array_reverse(ARRAY[1,2,3]);
