ff86778ff40cbf0ac4ae48e846cee820ef195553
Remi C <remi.fake@fake.com>
2014-07-25 09:38:39 +0200

    Adding a version able ot snap multi line to multiline

--- postgis/rc_SnapPointToLine.sql
+++ postgis/rc_SnapPointToLine.sql
@@ -74,4 +74,40 @@
 		--SELECT point, line, 1 AS tolerance, public.rc_SnapPointToLine(point  , line  , 1, 0.5  ) AS spoint 
 		--FROM ST_MakePoint(1,1) AS point, ST_GeomFromText('LINESTRING(0 0, 0 10, 0 100)') AS line
 
+
+
+
+		DROP FUNCTION IF EXISTS public.rc_SnapLineToLine(line_to_snap geometry, line geometry , tolerance double precision,tolerance_vertex double precision);
+	CREATE FUNCTION public.rc_SnapLineToLine(line_to_snap geometry, line geometry , tolerance double precision,tolerance_vertex double precision DEFAULT 0)
+		RETURNS geometry AS
+		$BODY$
+		-- This function snap all the points of a line ot another line: if under a given tolerance distance
+		--	if a vertex is close enough (tolerance_vertex), we snap to it
+		--	else, if the line is close enough, take the closest point on line (projection)
+		--	else, return the original point
+		DECLARE  
+		_cline geometry; 
 		
+		BEGIN
+				WITH d_lines AS (
+					SELECT dmp.geom, dmp.path As line_path
+					FROM rc_DumpLines(line_to_snap) AS dmp
+				)
+				,snapped_lines AS (
+					SELECT ST_Makeline(rc_SnapPointToLine(dmp.geom,line,tolerance, tolerance_vertex) ORDER BY dmp.path ASC) AS slines 
+					FROM d_lines, ST_DumpPoints( geom) AS dmp 
+					GROUP BY line_path 
+				)SELECT ST_Collect(slines) INTO _cline
+				FROM snapped_lines
+				LIMIT 1;
+		 
+			RETURN _cline;
+		END ;
+		$BODY$
+		LANGUAGE plpgsql IMMUTABLE;
+
+		SELECT  1 AS tolerance, ST_AsText(public.rc_SnapLineToLine(line1  , line2  , 0.6, 0.5  )) AS sline 
+		--FROM ST_GeomFromText('MULTILINESTRING((0.1 0, 1 10, 0.1 100),(0.2 0 , 1.5 10))') AS line1, ST_GeomFromText('LINESTRING(0 0, 0 10, 0 100)') AS line2
+		FROM ST_GeomFromText('LINESTRING(0.1 0, 1 10, 0.1 100)') AS line1, ST_GeomFromText('LINESTRING(0 0, 0 10, 0 100)') AS line2;
+
+		 
