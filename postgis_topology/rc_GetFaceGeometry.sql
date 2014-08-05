03fb4e27098ec09aebeac2ba6a24de302bc48389
Remi C <remi.fake@fake.com>
2014-08-05 18:31:14 +0200

    Nonn working version , will revert to use st_buildarea, it is too complicated !

--- postgis_topology/rc_GetFaceGeometry.sql
+++ postgis_topology/rc_GetFaceGeometry.sql
@@ -51,7 +51,7 @@ BEGIN
 				, left_face, right_face --only keeping it for final removing of isolated edges
 			FROM ' || quote_ident(toponame)||'.edge_data
 			WHERE edge_id != abs_next_left_edge --we avoid the (K,K) row because we would loop inifnitly in it
-				AND left_face = $1 	 
+				AND left_face = $1 	 AND right_face != $1
 		UNION ALL
 			--we have to invert the edges that are in the other direction ,so as everyone is turning in the same direction (counter clockwise)
 			SELECT edge_id ,abs_next_right_edge AS next_edge_id , ST_Reverse(geom) AS geom
@@ -59,6 +59,16 @@ BEGIN
 			FROM ' || quote_ident(toponame)||'.edge_data
 			WHERE edge_id != abs_next_right_edge --we avoid the (K,K) row because we would loop inifnitly in it
 				AND right_face = $1  
+		UNION ALL
+			--we have to invert the edges that are in the other direction ,so as everyone is turning in the same direction (counter clockwise)
+			SELECT DISTINCT ON (edge_id ) edge_id 
+				, CASE WHEN edge_id = abs_next_right_edge THEN  abs_next_left_edge
+						ELSE  abs_next_left_edge  END  AS next_edge_id 
+					, geom --ST_AddPoint(ST_AddPoint(geom,ST_EndPoint(geom),-1),ST_StartPoint(geom),-1) AS geom --adding the first point to the end of the geom
+				, left_face, right_face --only keeping it for final removing of isolated edges
+			FROM edge_data
+			WHERE (right_face = $1 OR left_face = $1)
+				AND (abs_next_right_edge != edge_id OR abs_next_left_edge = edge_id ) 
 	) 
 	,ordering AS (--we get an iteration column that gives the order for looping trough stuff. This way we know that the linestring are going to be put into the correct order
 		--note : it is possible to not compute if using buildarea with collected (unordered) edges. However BuildArea can t use the index that this ordering is using ! 
@@ -102,18 +112,17 @@ BEGIN
 	) 
 	 --we snap the beggining of each next line to the end of each edge_id line, then make a big line respecting order out of it, then make a polygon
 		SELECT  ST_MakePolygon( --taking the line to make a polygon with it
-					ST_LineMerge( --merging the multiline into one continuous line
-						ST_Collect( --grouping the line into one __respecting the order__ , the order is paramount
-							ST_SetPoint(--setting the first point of next_line to be the same as last point from line
-								geom2
-								,0
+					ST_MakeLine( --merging the multiline into one continuous line
+						  --grouping the line into one __respecting the order__ , the order is paramount
+							ST_AddPoint(--setting the first point of next_line to be the same as last point from line
+								geom2 
 								, ST_PointN(
 									geom1
 									, ST_NPoints(geom1)
 									)
-								) 
-							ORDER BY ordinality ASC)
-						)
+								,0) 
+								
+							ORDER BY ordinality ASC) 
 					)  AS geom
 		FROM rebuilding_order
 		LIMIT 1-- this is not necessary but it is to be sure
@@ -147,7 +156,8 @@ COMMENT ON FUNCTION public.rc_getfacegeometry(character varying, integer) IS 'ar
 		FROM face
 		WHERE face_id !=0
 		ORDER BY face_id ASC 
-		LIMIT 1000
+		LIMIT 500 
+		OFFSET 0
 	)
 	,rc AS (
 		SELECT rc_getfacegeometry('bdtopo_topological', face_id)
@@ -168,23 +178,39 @@ FROM face
 WHERE face_id != 0
 LIMIT 100
 
-
+SELECT *
+FROM edge_data
+WHERE right_face = 179 OR left_face = 179
 
 
 WITH n_edge AS ( --getting all the edges that are at the border of the face
 	SELECT edge_id ,abs_next_left_edge AS next_edge_id , geom
 		, left_face, right_face --only keeping it for final removing of isolated edges
 	FROM edge_data
-	WHERE left_face = 270 	
+	WHERE left_face = 179 AND right_face != 179
 		AND abs_next_left_edge != edge_id --avoiding the (K,K) edge, becauses it causes the recursive cte to run infinitively
 	UNION ALL
 	--we have to invert the edges that are in the other direction ,so as everyone is turning in the same direction (counter clockwise)
 	SELECT edge_id ,abs_next_right_edge AS next_edge_id , ST_Reverse(geom) AS geom 
 		, left_face, right_face --only keeping it for final removing of isolated edges
 	FROM edge_data
-	WHERE right_face = 270 
+	WHERE right_face = 179 
 		AND abs_next_right_edge != edge_id --avoiding the (K,K) edge, becauses it causes the recursive cte to run infinitively
+
+	UNION ALL
+	--we have to invert the edges that are in the other direction ,so as everyone is turning in the same direction (counter clockwise)
+	SELECT DISTINCT ON (edge_id ) edge_id
+		, CASE WHEN edge_id = abs_next_right_edge THEN  abs_next_left_edge
+				ELSE  abs_next_left_edge  END  AS next_edge_id 
+			, ST_AddPoint(ST_AddPoint(geom,ST_EndPoint(geom),-1),ST_StartPoint(geom),-1) AS geom --adding the first point to the end of the geom
+		, left_face, right_face --only keeping it for final removing of isolated edges
+	FROM edge_data
+	WHERE (right_face = 179 OR left_face =179)
+		AND (abs_next_right_edge != edge_id OR abs_next_left_edge = edge_id ) 
 	) 
+	-- SELECT edge_id, next_edge_id--, ST_AsText(ST_Collect(geom))
+-- 	FROM n_edge
+-- 	ORDER BY edge_id ASC
 	,ordering AS (--we get an iteration column that gives the order for looping trough stuff. This way we know that the linestring are going to be putted into the correct order
 		--note : it is possible to not compute if using buildarea with collected (unordered) edges.
 		SELECT * 
@@ -213,7 +239,7 @@ WITH n_edge AS ( --getting all the edges that are at the border of the face
 			
 		FROM ordering AS o
 			INNER JOIN n_edge AS e1 ON (e1.edge_id = o.edge_id ) 
-		WHERE  e1.left_face != e1.right_face --we don't use the isolated edges, because it will have no impact on the polygon
+	--	WHERE  e1.left_face != e1.right_face --we don't use the isolated edges, because it will have no impact on the polygon
 	)
 	,rebuilding_order AS (--we have to recompute next_edge because some edges may be filtered out because they won't participate 
 		SELECT edge_id
@@ -229,13 +255,13 @@ WITH n_edge AS ( --getting all the edges that are at the border of the face
 			ST_MakePolygon( --taking the line to make a polygon with it
 				ST_MakeLine( --merging the multiline into one continuous line,  __respecting the order__ , the order is paramount
 					 
-						ST_SetPoint(--setting the first point of next_line to be the same as last point from line
+						ST_AddPoint(--setting the first point of next_line to be the same as last point from line
 							geom2
-							,0
 							, ST_PointN(
 								geom1
 								, ST_NPoints(geom1)
-								)
+								) 
+							,0
 							) 
 						ORDER BY ordinality ASC)
 					)
@@ -249,4 +275,6 @@ FROM face
 LIMIT 1000
 
 
-	 SELECT ST_Astext(st_getfacegeometry('bdtopo_topological', 102))
+	 SELECT ST_Astext(rc_getfacegeometry('bdtopo_topological', 735))
+	 SELECT ST_Astext(st_getfacegeometry('bdtopo_topological', 735))
+	 
