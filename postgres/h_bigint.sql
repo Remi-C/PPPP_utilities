fb169ccf432bf015b848f97b0e3449f0c659e3f3
Remi C <remi.cura@gmail.com>
2014-08-12 17:55:28 +0200

    new hash function

---
+++ postgres/h_bigint.sql
@@ -0,0 +1,11 @@
+﻿
+--found on internet : create a bigint has out of a text
+
+create or replace function h_bigint(text) returns bigint as $$
+ select ('x'||substr(md5($1),1,16))::bit(64)::bigint;
+$$ language sql;
+
+
+create or replace function h_int(text) returns int as $$
+ select ('x'||substr(md5($1),1,8))::bit(32)::int;
+$$ language sql;
