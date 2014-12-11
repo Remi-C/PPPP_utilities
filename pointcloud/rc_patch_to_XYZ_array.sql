527e118f2734c399d67cbf2964aafeb4c51b187c
Remi C <remi.fake@fake.com>
2014-12-11 11:13:23 +0100

    new functions

---
+++ pointcloud/rc_patch_to_XYZ_array.sql
@@ -0,0 +1,37 @@
+﻿-----------------------------------------------------------
+--
+--Rémi-C , Thales IGN
+--11/2014
+-- 
+  ----------------------
+
+
+	--we need an array agg for array, found in PPPP_utilities
+			DROP AGGREGATE public.array_agg_custom(anyarray) ;
+			CREATE AGGREGATE public.array_agg_custom(anyarray)
+				( SFUNC = array_cat,
+				STYPE = anyarray
+				);
+
+	--a wrapper function to convert from patch to array[array[]], so to be able to transmit information	
+	DROP FUNCTION IF EXISTS rc_patch_to_XYZ_array(ipatch PCPATCH,maxpoints INT, int);
+	CREATE OR REPLACE FUNCTION rc_patch_to_XYZ_array(ipatch PCPATCH,maxpoints INT DEFAULT 0, rounding_digits int default 3
+		)
+	  RETURNS FLOAT[] AS
+	$BODY$
+			--@brief this function clean result tables
+			-- @return :  nothing 
+			DECLARE 
+			BEGIN 
+				RETURN array_agg_custom(
+					ARRAY[
+						round(PC_Get(pt.point,'X'),rounding_digits)
+						, round(PC_Get(pt.point,'Y'),rounding_digits)
+						, round(PC_Get(pt.point,'Z'),rounding_digits)
+					] ORDER BY pt.ordinality ASC )
+				FROM public.rc_ExplodeN_numbered(  ipatch,maxpoints) as pt ; 
+			END ; 
+		$BODY$
+	LANGUAGE plpgsql IMMUTABLE STRICT;
+	--SELECT rc_patch_to_XYZ_array()
+ 
---
+++ postgres/rc_array_padding.sql
@@ -0,0 +1,24 @@
+﻿-----------------------------
+--Rémi C
+--12/2014 
+---------------------------------------
+
+
+	--creating function
+DROP FUNCTION IF EXISTS rc_array_padding(iarr anyarray ,  start_indice int, final_arr_length int , OUT oarr anyarray) ; 
+CREATE OR REPLACE FUNCTION  rc_array_padding(iarr anyarray , start_indice int, final_arr_length int ,  OUT oarr anyarray)
+AS $$ 
+-- @brief : this function takes an array and fill it with 0 to reach desired size
+DECLARE 
+	i int  ;
+	last_indice int := LEAST(array_length(iarr,1),final_arr_length );
+BEGIN
+	oarr := array_fill(0, ARRAY[final_arr_length  ]);
+
+	for i in 1..final_arr_length 
+		LOOP
+			oarr[i] := COALESCE(iarr[i+start_indice-1],0) ; 
+		END LOOP; 
+RETURN ; 
+END;
+$$ LANGUAGE 'plpgsql' IMMUTABLE STRICT ;
