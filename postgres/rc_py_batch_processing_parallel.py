41cf71a0163f932e47af255d604ce40c961ff03e
Remi C <remi.fake@fake.com>
2014-12-16 19:07:01 +0100

    new python function to laucnh parallel processing on postgres server

---
+++ postgres/rc_py_batch_processing_parallel.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+this script allow to lauch sql query the parallel <ay cutting the data set into slides
+we exploit a pkey gid, int, ideally serial
+"""
+
+
+import psycopg2
+
+import  multiprocessing as mp; 
+import time;  
+import random; 
+from random import shuffle
+
+  
+  
+
+  
+
+ 
+def execute_querry(cur, i,j):
+    """ this function should execute the querry on the range of gid proposed """
+    q = """
+    UPDATE  vosges_2011.las_vosges as rps
+	SET (patch,points_per_level) = ( nv.opatch, nv.points_per_level)
+	FROM  (
+		SELECT p.gid, r.opatch, r.points_per_level
+		FROM vosges_2011.las_vosges as p 
+			,rc_order_octree( p.patch , 8) as r  
+		WHERE gid  BETWEEN %s AND %s  
+        AND p.points_per_level IS NULL
+		) as nv
+		WHERE nv.gid  = rps.gid ; 
+    """ ;
+    print q % (i,j) ;  
+    cur.execute( q ,( i, j))
+     
+#execute query  
+     
+     
+
+def batch_LOD_monoprocess((key_min,key_max,key_step)): 
+    """ this function connect to databse, and execute the querry on the specified gid range, step by step"""    
+    #connect to db
+    conn = psycopg2.connect(  
+        database='vosges'
+        ,user='postgres'
+        ,password='postgres'
+        ,host='172.16.3.50'
+        ,port='5432' ) ; 
+    cur = conn.cursor()
+
+    #setting the search path    
+    cur.execute("""SET search_path to vosges_2011, public;"""); 
+    conn.commit(); 
+     
+    i = key_min; 
+    while i < key_max :
+        #print i;
+        #print i, i+key_step;
+        execute_querry(cur, i,i+key_step)
+        conn.commit() 
+        i+=key_step ; 
+    cur.close()
+    conn.close()
+ 
+ 
+def batch_LOD_monoprocess_test( ):
+    """utility debug function"""
+    key_min= 10000
+    key_max = 10010
+    key_step = 2
+    batch_LOD_monoprocess(key_min,key_max,key_step) ;
+
+#batch_LOD_monoprocess_test();
+
+def split_interval_into_smaller_interval(split_number,key_tot_start,key_tot_end, the_step):
+    """ simply takes a big interval and split it into small pieces. Warning, possible overlaps of 1 elements at the beginning/end"""
+    key_range = abs(key_tot_end-key_tot_start)/(split_number *1.0) 
+    interval_to_process = [] ;
+    for i,(proc) in enumerate(arange(1,split_number+1)):
+        key_local_start = int(ceil(key_tot_start+i * key_range)) ;
+        key_local_end = int(trunc(key_tot_start+(i+1) * key_range)); 
+        interval_to_process.append([ key_local_start , key_local_end, the_step])        
+        #batch_LOD_monoprocess(key_min,key_max,key_step):
+    print interval_to_process
+    return interval_to_process
+    
+
+def batch_LOD_multiprocess(processes,split_number,key_tot_start,key_tot_end,the_step): 
+    """Main function, execute a given query in parallel on server"""
+    #splitting the start_end into split_number interval
+    subintervals = split_interval_into_smaller_interval(split_number,key_tot_start,key_tot_end,the_step);
+    #shuffle so that subintervals are in random order
+    shuffle(subintervals); 
+    
+    #multiprocessing: 
+    pool = mp.Pool(processes); 
+    results = pool.map(batch_LOD_monoprocess, subintervals)
+    return results
+
+def batch_LOD_multiprocess_test():
+    """ test of the main function, parameters adapted to IGN big server"""
+    split_number = 1000; 
+    key_tot_start=8736
+    key_tot_end=590264
+    key_step = 10 ;
+    processes = 20 ;
+    
+    batch_LOD_multiprocess(processes,split_number,key_tot_start,key_tot_end,key_step); 
+    
+        
+
+batch_LOD_multiprocess_test();
+
+  
+ 
