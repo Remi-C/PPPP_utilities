42d8053db0935c4016f374108086c20ca8f3397e
Remi C <remi.fake@fake.com>
2015-02-13 19:10:17 +0100

    correcting the script to be edge and not node, starting to write first easy cases

--- postgis_topology/editing_in_qgis/editing_edge_in_qgis.sql
+++ postgis_topology/editing_in_qgis/editing_edge_in_qgis.sql
@@ -7,72 +7,37 @@
 -- aim of this project is to support limited edition of a linestring topology in qgis, via triggers on postgis topology 
 
 /**
---scope : only allowed modifications : on edge_data (view) and node (view)
-
-Change on node :
-	CREATE :
-		_if isolated : create isolated 
-		-if along an existing edge : split existing edge, update both new edge
-		- else : forbiden
-	UPDATE : 
-		move : move all adjacent edges last point, if  no crossing
-		if crossing : forbiden
-	DELETE :
-		if isolated ,
-		else : forbiden
-
-Change on edge :
-	CREATE 
-		_ allowed between 2 nodes, non crossing
-		else forbiden
-	UPDATE 
-		change geometry : all except last/first : if not crossing
-		chang geometry : last OR first : update topo, 
-		change geometry : last and first ! forbiden
-	DELETE 
-		propagate
-
-Change on edge_data :
-	on update or create, check that it is not crossing
-	check that both ends are within precision of a node, snap, error if false
-	check that edge is unique fo given precision (closest looking edge max distance is > precision)
-Change on node : 
-	on update / create  : if node dwihtin precision, merge
-	if edgedwithin precision, split
-	
-		
+--scope : only allowed modifications : on edge_data (view) and node (view) 
 */
 
---creating view for edition
- 
-
+--creating view for edition 
 DROP VIEW IF EXISTS bdtopo_topological.edge_editing CASCADE; 
 CREATE VIEW  bdtopo_topological.edge_editing  AS (
-	SELECT edge_id, geom as edge_geom
+	SELECT edge_id, ST_Force2D(geom) as edge_geom
 	FROM bdtopo_topological.edge_data
 ); 
 
+SELECT *
+FROM bdtopo_topological.edge_editing ;
 
---creating trigger for edition of node
+--INSERT INTO bdtopo_topological.edge_editing (edge_geom ) VALUES (ST_GeometryFromText('LINESTRING(1451.8 21353.4 , 1447.6 21253.7)',932011)) ; 
+--creating trigger for edition of edge
 CREATE OR REPLACE FUNCTION rc_edit_edge_topology(  )
   RETURNS  trigger  AS
 $BODY$  
 	/**
-	@brief this trigger is designed to allow simple update of edge topology via qgis
-
+	@brief this trigger is designed to allow simple update of edge topology via qgis 
 	allowed interaction are :
-		delete edges : simply delete the edge
-		create edge : create node if necessary, then create edge
-		update edge : create node if necessary, update edge, check that proposed update is valid (no crossing).  
-	 
-	*/
-
+		delete edges : simply delete the edge, don't delete node (std)
+		create edge : create node if necessary, then create edge between 2 nodes (std)
+		update edge : (most complicated) 
+			create node if necessary, update edge, check that proposed update is valid (no crossing).   
+	*/ 
 	DECLARE     
-	BEGIN      
-
+	BEGIN  
 	IF TG_OP = 'DELETE' THEN  
-		SELECT f.deleted_node_id , f.deleted_node_geom INTO OLD.node_id, OLD.node_geom
-		FROM topology.rc_DeleteEdgeSafe(TG_TABLE_SCHEMA::text, OLD.node_id,OLD.node_geom)   as f ; 
+		SELECT f.deleted_edge_id , f.deleted_edge_geom INTO NEW.edge_id, NEW.edge_geom
+		FROM topology.rc_DeleteEdgeSafe(TG_TABLE_SCHEMA::text,NEW.edge_id,NEW.edge_geom)   as f ; 
 		 
 		RETURN NULL ; 
 		--returN NEW;
@@ -80,9 +45,10 @@ $BODY$
 
 	IF TG_OP = 'INSERT' THEN 
 		--update/insert case
-		NEW.node_geom = ST_Force3D(NEW.node_geom) ;  --safeguard against qgis
-		SELECT f.inserted_node_id , f.inserted_node_geom INTO NEW.node_id, NEW.node_geom
-		FROM topology.rc_InsertEdgeSafe(TG_TABLE_SCHEMA::text, NEW.node_id,NEW.node_geom)   as f ; 
+		NEW.edge_geom = ST_Force3D(NEW.edge_geom) ;  --safeguard against qgis
+		
+		SELECT f.inserted_edge_id , f.inserted_edge_geom INTO NEW.edge_id, NEW.edge_geom
+		FROM topology.rc_InsertEdgeSafe(TG_TABLE_SCHEMA::text, NEW.edge_id,NEW.edge_geom)   as f ; 
 		 
 		RETURN NULL ;  
 	END IF ; --end of insert dealing
@@ -98,7 +64,7 @@ $BODY$
 		--returN NEW;
 	END IF ; --end of insert dealing
 
-	returN NEW;
+	returN NULL; 
 	END ;
 	$BODY$
   LANGUAGE plpgsql VOLATILE;
@@ -112,20 +78,47 @@ EXECUTE PROCEDURE rc_edit_edge_topology();
  
 
 
-DROP FUNCTION IF EXISTS topology.rc_InsertEdgeSafe(topology_name text , new_node_id int ,new_geom geometry , OUT inserted_node_id int, OUT inserted_node_geom geometry)  ;
-CREATE OR REPLACE FUNCTION topology.rc_InsertEdgeSafe(topology_name text , new_node_id int ,new_geom geometry , OUT inserted_node_id int, OUT inserted_node_geom geometry)  AS
+DROP FUNCTION IF EXISTS topology.rc_InsertEdgeSafe(topology_name text , new_edge_id int ,new_geom geometry , OUT inserted_edge_id int, OUT inserted_edge_geom geometry)  ;
+CREATE OR REPLACE FUNCTION topology.rc_InsertEdgeSafe(topology_name text , new_edge_id int ,new_geom geometry , OUT inserted_edge_id int, OUT inserted_edge_geom geometry)  AS
 $BODY$  
 	/**
 	@brief this function safely add a edge to a topology.
+		--for each end, safely insert a node (take care of near node and near edge)
+		-- create an edge between 
 		 
 	*/ 
 	DECLARE    
-		_topology_precision float := 0 ;  
+		_topology_precision float := 0 ; 
+		_first_node record;  
+		_second_node record;
+		_second_node_geom geometry; 
+		_e_geom geometry ;
 	BEGIN    
 		SELECT precision into _topology_precision
 		FROM topology.topology
 		WHERE name = topology_name  ;   
-	 
+
+		--find a sequence number of node
+		--inserting the first node if necessary
+		SELECT inserted_node_id as node_id, inserted_node_geom as geom INTO _first_node
+		FROM topology.rc_InsertNodeSafe(topology_name
+			,  rc_FindNextValue(topology_name, 'node', 'node_id')   
+			, ST_StartPoint(new_geom)); 
+
+		--inserting the second node if necessary
+		SELECT inserted_node_id as node_id, inserted_node_geom as geom INTO _second_node
+		FROM topology.rc_InsertNodeSafe(topology_name
+			,  rc_FindNextValue(topology_name, 'node', 'node_id')   
+			, ST_EndPoint(new_geom)); 
+
+		--updating the edge geom, so it correctly begins and end on added node
+		_e_geom := rc_SetPoint(rc_SetPoint(new_geom,0, _first_node.geom),-1, _second_node.geom) ;
+		
+		--creating the edge between both nodes
+		SELECT ST_AddEdgeModFace(topology_name , _first_node.node_id, _second_node.node_id, _e_geom) INTO inserted_edge_id; 
+		inserted_edge_geom := _e_geom; 
+
+		RETURN ;  
 	END ;
 	$BODY$
   LANGUAGE plpgsql VOLATILE;
@@ -142,6 +135,7 @@ $BODY$
 		SELECT precision into _topology_precision
 		FROM topology.topology
 		WHERE name = topology_name  ;    
+
 		RETURN  ;
 	END ;
 	$BODY$
@@ -153,9 +147,12 @@ CREATE OR REPLACE FUNCTION topology.rc_DeleteEdgeSafe(topology_name text , INOUT
 $BODY$  
 	/**
 	@brief this function safely delete an edge from a topology. 
+	it is actually jsut a wrapper around already safe ST_RemEdgeModFace
+	NOTE : it could be changeed to delete isolated node when deleting edge
 	*/ 
 	DECLARE  
-	BEGIN     	 
+	BEGIN     	  
+		SELECT ST_RemEdgeModFace(topology_name,deleted_node_id)  INTO deleted_node_id ; 
 		return ; 
 	END ;
 	$BODY$
