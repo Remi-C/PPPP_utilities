301e417b561a5eed12d7499812f37abbb5bd7fc1
Remi C <remi.fake@fake.com>
2015-02-16 16:35:30 +0100

    rewriting the function to really use the topology_name provided

--- postgis_topology/rc_CheckNewEdgeGeom.sql
+++ postgis_topology/rc_CheckNewEdgeGeom.sql
@@ -19,27 +19,34 @@ $BODY$
 		_e_geom geometry ;
 		_e_id int[];
 		_edge_id int:= edge_id ;
+		_q text ; 
 	BEGIN     	  
-		-- chack that the proposed geometry is simple
+		-- check that the proposed geometry is simple
 		IF ST_IsSImple(edge_geom) = FALSE OR ST_IsValid(edge_geom) = FALSE THEN
 			RAISE EXCEPTION 'ERROR : the given edge % is not valid or self intersect (not simple)\n',edge_id ;
 		END IF; 
 		-- check that new geom doesn't cross any edge (except OLD self)
-		SELECT array_agg(ed.edge_id ORDER BY ed.edge_id ASC) INTO _e_id
-		FROM bdtopo_topological.edge_data as ed 
-		WHERE ST_Crosses(edge_geom,geom) = TRUE
-			AND ed.edge_id != _edge_id ;
+
+		_q := format('
+		SELECT array_agg(ed.edge_id ORDER BY ed.edge_id ASC) 
+		FROM %I.edge_data as ed 
+		WHERE ST_Crosses($1,geom) = TRUE
+			AND ed.edge_id != $2 ;
+		' ,topology_name); 
+		EXECUTE _q INTO _e_id  USING  edge_geom, _edge_id;
 
 		if _e_id iS NOT NULL THEN
 			RAISE EXCEPTION 'ERROR : the given edge % cross other edges : % ',edge_id, _e_id; 
 		END IF; 
 
 		--check that new edge geom is not too close to node 
-		SELECT array_agg(n.node_id ORDER BY n.node_id ASC ) INTO _e_id
-		FROM bdtopo_topological.node as n 
-		WHERE ST_DWithin(edge_geom,n.geom,tolerance) = TRUE
-			AND n.node_id <> start_node_id
-			AND n.node_id <> end_node_id;  
+		_q := format('
+		SELECT array_agg(n.node_id ORDER BY n.node_id ASC ) 
+		FROM %I.node as n 
+		WHERE ST_DWithin($1,n.geom,$4) = TRUE
+			AND n.node_id <> $2
+			AND n.node_id <> $3;  ',topology_name);  
+			EXECUTE _q INTO _e_id USING edge_geom,start_node_id, end_node_id,tolerance ; 
 		
 		if _e_id iS NOT NULL THEN
 			RAISE EXCEPTION 'ERROR : the given edge % is to close to other existing node : % ',edge_id, _e_id; 
@@ -49,7 +56,7 @@ $BODY$
 	END ;
 	$BODY$
 LANGUAGE plpgsql VOLATILE; 
-/*
+ 
 SELECT topology.rc_CheckNewEdgeGeom('bdtopo_topological'::text
 	, ST_SetSRID(ST_MakeLine(ST_MakePoint(5983.5,22331.1), ST_MakePoint(6037.9,22309.9)),932011)
 	,-1::int
@@ -57,4 +64,4 @@ SELECT topology.rc_CheckNewEdgeGeom('bdtopo_topological'::text
 	,-1::int
 	,0.1::float
 	)
- */
+ 
