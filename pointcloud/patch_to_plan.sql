6e7414a70121ba96fe478932fabf472a32bcd991
Remi-C <Remi-C@fake.fake>
2015-04-06 14:52:23 +0200

    few update

--- pointcloud/patch_to_plan.sql
+++ pointcloud/patch_to_plan.sql
@@ -1,7 +1,10 @@
 ﻿
+
+
+
 	--a plpython function taking the array of double precision and converting it to pointcloud, then looking for planes inside, then cylinder
 	--note that we could do the same to detect cylinder
-DROP FUNCTION IF EXISTS rc_py_plane_and_cylinder_detection_from_patch ( pcpatch,  INT,INT,  FLOAT,INT,FLOAT,FLOAT,INT);
+DROP FUNCTION IF EXISTS rc_py_plane_and_cylinder_detection_from_patch ( pcpatch,  INT,INT,  FLOAT,FLOAT,FLOAT,INT);
 CREATE FUNCTION rc_py_plane_and_cylinder_detection_from_patch (
 	uncompressed_pcpatch pcpatch
 	,plane_min_support_points INT DEFAULT 4
@@ -29,9 +32,14 @@ then iteratively finding plan in the cloud using ransac
 import numpy as np
 import pcl 
 import sys
-sys.path.insert(0, '/home/remi/PPPP_utilities/') 
-import patch_to_plan as ptp
 
+sys.path.insert(1, '/media/sf_perso_PROJETS/PPPP_utilities/pointcloud') 
+
+
+import patch_to_plan as ptp
+reload(ptp)
+import pg_pointcloud_classes as pgp
+reload(pgp)
 connection_string = """host=localhost dbname=test_pointcloud user=postgres password=postgres port=5433"""
 GD = pgp.create_GD_if_not_exists()
 pgp.create_schemas_if_not_exists()
@@ -45,30 +53,43 @@ result , p_reduced = ptp.perform_N_ransac_segmentation(
 	    p
 	    ,plane_min_support_points
 	    ,plane_max_number
-	    , plane_ksearch
 	    , plane_search_radius
-	    , pcl.SACMODEL_NORMAL_PLANE
+	    , pcl.SACMODEL_PLANE
 	    , plane_distance_weight
 	    , plane_max_iterations
 	    , plane_distance_threshold) ;
 
+plpy.notice(p_reduced)
 
 return result ; 
 $$ LANGUAGE plpythonu IMMUTABLE STRICT; 
 
 WITH pa AS (
-	SELECT gid, pc_uncompress(patch) as u_patch
+	SELECT gid, pc_uncompress(patch) as u_patch , pc_numpoints(patch) --, * 
 	FROM benchmark_cassette_2013.riegl_pcpatch_space
+	WHERE -- pc_numpoints(patch) BETWEEN 100 AND 110 --1000 AND 5000
+		 gid  = 918
+		--AND gid = 4523
 	LIMIT 1  
+	
 )
-SELECT r.*
+SELECT gid, r.*
 FROM pa ,rc_py_plane_and_cylinder_detection_from_patch (
 	u_patch
-	,plane_min_support_points := 4
-	,plane_max_number := 100
+	,plane_min_support_points := 100
+	,plane_max_number := 10
 	,plane_distance_threshold := 0.1
-	,plane_ksearch := 50
-	,plane_search_radius := 0.1
+	,plane_ksearch := 10
+	,plane_search_radius := 1
 	,plane_distance_weight := 0.5 --between 0 and 1 . 
-	,plane_max_iterations := 100 
+	,plane_max_iterations := 10000
 	) as r
+-- 
+-- 
+-- COPY (
+-- 		SELECT pc_uncompress(patch) as u_patch 
+-- 	FROM benchmark_cassette_2013.riegl_pcpatch_space
+-- 	WHERE gid = 4523
+-- )
+-- TO '/tmp/test_patch'
+
