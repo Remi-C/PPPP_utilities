57d09bf4c1718c9bd423652237dc85ef493e1ef6
Remi-C <***@***.com>
2015-06-19 17:35:43 +0200

    new function, allow to print variable during query execution

---
+++ postgres/rc_msg.sql
@@ -0,0 +1,41 @@
+﻿---------------------------------------------
+--Copyright Remi-C Thales & IGN , Terra Mobilita Project, 2014
+--
+---------------------------------------------- 
+--  2 functions usefull to debug complex query, allow to print the content of a variable
+--the choice between vloatile and immutable allow to choose wether the function is called on time or several
+--------------------------------------------
+		 
+DROP FUNCTION IF EXISTS rc_msg_vol(text);
+CREATE OR REPLACE FUNCTION rc_msg_vol(   
+		 i_text TEXT 
+	 ) RETURNS VOID AS 
+	$BODY$
+		DECLARE     
+		BEGIN 
+		--@brief this function print a message and the associated time
+		--@return nothing
+
+			RAISE NOTICE  '%: %',clock_timestamp(), i_text; 
+			RETURN ;
+		END ;  
+	$BODY$
+LANGUAGE plpgsql VOLATILE CALLED ON NULL INPUT; 
+
+
+DROP FUNCTION IF EXISTS rc_msg_immutable(text);
+CREATE OR REPLACE FUNCTION rc_msg_immutable(   
+		 i_text TEXT 
+	 ) RETURNS VOID AS 
+	$BODY$
+		DECLARE     
+		BEGIN 
+		--@brief this function print a message and the associated time
+		--@return nothing
+
+			RAISE NOTICE  '%: %',clock_timestamp(), i_text; 
+			RETURN ;
+		END ;  
+	$BODY$
+LANGUAGE plpgsql IMMUTABLE  CALLED ON NULL INPUT; 
+ 
