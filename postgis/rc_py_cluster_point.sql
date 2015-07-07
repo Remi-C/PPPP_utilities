db2f7e1675bb180ae93331a8d8947b6db16e10fa
Remi-C <***@***.com>
2015-07-07 16:17:07 +0200

    adding a function to cluster in 3D with dbscan

--- postgis/rc_py_cluster_point.sql
+++ postgis/rc_py_cluster_point.sql
@@ -30,3 +30,26 @@ return result;
 $$ LANGUAGE plpythonu IMMUTABLE STRICT; 
 
 
+--a plpython predicting gt_class, cross_validation , result per class
+--gids,feature_iar,gt_classes ,labels,class_list,k_folds,random_forest_ntree, plot_directory
+DROP FUNCTION IF EXISTS rc_py_cluster_points_dbscan( igid int[], dat FLOAT[] , ndim int,  max_dist float, min_sample int );
+CREATE FUNCTION rc_py_cluster_points_dbscan( igid int[],dat FLOAT[] ,ndim int,max_dist float, min_sample int )
+RETURNS table(gid int, label int)
+AS $$"""
+This function ake input, 
+"""
+import numpy as np
+from scipy import cluster
+from sklearn import cluster
+data_iar = np.array( dat, dtype=np.float)
+gids = np.array( igid, dtype=np.int32)
+vector_points = np.reshape( data_iar,( len(data_iar)/ndim, ndim )) ; 
+
+clust = cluster.DBSCAN(eps=max_dist, min_samples=5, metric='euclidean', algorithm='auto', leaf_size=30, p=None, random_state=None) 
+label = clust.fit_predict(vector_points)
+result = [] 
+for i in range(0,len(label)):
+    result.append(( (gids[i]) , (label[i]) )) ; 
+return result;
+$$ LANGUAGE plpythonu IMMUTABLE STRICT; 
+
