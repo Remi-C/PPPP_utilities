fd5cd0919320ec72f57e8e66a122256e001cbc4e
Remi C <remi.fake@fake.com>
2015-09-01 16:45:08 +0200

    working version, with full test case

--- postgis/rc_generate_parallelogram.sql
+++ postgis/rc_generate_parallelogram.sql
@@ -20,7 +20,7 @@ DROP FUNCTION IF EXISTS rc_generate_parallelogram(
 	, IN alpha float
 	,IN  support_line_size FLOAT
 	,OUT opoint geometry
-	 );
+	 )  ;
 
 
 	  
@@ -30,12 +30,13 @@ CREATE OR REPLACE FUNCTION rc_generate_parallelogram(
 	, IN ipoint2 geometry 
 	, IN buffer geometry
 	, IN  width FLOAT
-	, IN alpha float
+	, IN alpha float --expected ot be in degrees
 	,IN  support_line_size FLOAT DEFAULT 0.1  
 	, OUT osurf geometry
 	 ) AS
 $BODY$
 	/** from 2 points, project left and right of axis on buffer, then create a surf out of 2 sublines.
+	WARNING  angle is in degree
 	*/
 	DECLARE
 		_ipb1_l geometry ;
@@ -53,26 +54,41 @@ $BODY$
 		--project ipp1 and ipp2 on left and right of buffer
 		--extract substrings of buffer
 		--link substring to create a surface
+		buffer := ST_ExteriorRing(buffer ) ; 
 
 		ipoint1 := ST_CLosestPoint(iline, ipoint1) ;
-		ipoint2 := ST_CLosestPoint(iline, ipoint2) ;
-		
+		ipoint2 := ST_CLosestPoint(iline, ipoint2) ; 
+ 
+		WITH i_data AS (
+			SELECT iline AS _iline
+			,  ipoint1  AS _ipoint1
+			,  ipoint2 AS _ipoint2
+			,  buffer  AS _buffer
+			, width AS _width
+			, radians(alpha) AS _alpha
+			, support_line_size AS _support_line_size
+		)
+		,projecting_points AS ( --projecting points on buffer
+			SELECT  rc_lib.rc_project_point_on_buffer(_iline, _ipoint1, _buffer, _width, _alpha, _support_line_size)   AS ipb1_l
+				 ,  rc_lib.rc_project_point_on_buffer(_iline, _ipoint1, _buffer, -_width, _alpha, _support_line_size)  AS ipb1_r
+				,   rc_lib.rc_project_point_on_buffer(_iline, _ipoint2, _buffer, _width, _alpha, _support_line_size)   AS ipb2_l
+				,   rc_lib.rc_project_point_on_buffer(_iline, _ipoint2, _buffer, -_width, _alpha, _support_line_size)  AS ipb2_r
+			FROM i_data
+		)  
+		--,converting_to_curv_abs As ( -- converting each projected point to  a curvilinear abcisia
+		SELECT ARRAY[ST_LineLocatePoint(_buffer, ipb2_l)  
+			, ST_LineLocatePoint(_buffer, ipb1_l) 
+			,ST_LineLocatePoint(_buffer, ipb1_r) 
+			, ST_LineLocatePoint(_buffer, ipb2_r)  ]
+			INTO _abs 
+		FROM i_data , projecting_points ;
 
-		_ipb1_l  := rc_project_point_on_buffer(iline, ipoint1, buffer, width, alpha, support_line_size) ; 
-		_ipb1_r  := rc_project_point_on_buffer(iline, ipoint1, buffer, -width, alpha, support_line_size) ;
-		_ipb2_l  := rc_project_point_on_buffer(iline, ipoint2, buffer, width, alpha, support_line_size) ;
-		_ipb2_r  := rc_project_point_on_buffer(iline, ipoint2, buffer, -width, alpha, support_line_size) ; 
+		--RAISE EXCEPTION '_abs % ',_abs; 
 
-		buffer := ST_ExteriorRing(buffer)  ; 
-		--extracting substrings 
-		SELECT array_agg(abs ORDER BY side, cat ) INTO _abs
-		FROM (
-		SELECT ST_LineLocatePoint(buffer,_ipb2_l) as abs, 2 AS cat, 'l' AS side
-		UNION ALL  SELECT ST_LineLocatePoint(buffer,_ipb1_l)  ,1 AS cat, 'l' AS side
-		UNION ALL  SELECT ST_LineLocatePoint(buffer,_ipb2_r)  ,2 AS cat, 'r' AS side
-		UNION ALL  SELECT ST_LineLocatePoint(buffer,_ipb1_r) ,1 AS cat, 'r' AS side
-		) AS sub ; 
+		_substr_l := rc_lib.rc_circularsubstring(buffer, _abs[1], _abs[2]) ;
+		_substr_r := rc_lib.rc_circularsubstring(buffer, _abs[3], _abs[4]) ;
 
+		/*
 		IF _abs[1]>_abs[2] THEN
 			_substr_l := ST_Reverse(ST_LineSubstring(buffer, _abs[2], _abs[1]))  ;
 		ELSIF _abs[3]>_abs[4] THEN
@@ -81,8 +97,8 @@ $BODY$
 			_substr_l := ST_LineSubstring(buffer, _abs[1], _abs[2]) ;
 			_substr_r := ST_LineSubstring(buffer, _abs[3], _abs[4]) ;
 		END IF;
-
-		--RAISE EXCEPTION '_abs %', _abs;
+		*/
+		
 		
 		--RAISE EXCEPTION 'coucou' ; 
 		--sewing substring 
@@ -94,55 +110,69 @@ $BODY$
 	$BODY$
 LANGUAGE plpgsql IMMUTABLE STRICT; 
  
-/*
-DROP TABLE IF EXISTS temp_test ;
-CREATE TABLE temp_test AS 
-SELECT 1 AS gid, ipoint, iline, ibuff, f 
-FROM  ST_MakePoint(1 ,1)  AS ipoint
-	, ST_GeomFromText('LINESTRING(-10 -10, 10 10)') as iline 
-	, ST_Buffer(iline, 4 ) as ibuff
-	, rc_project_point_on_buffer(iline, ipoint, ibuff, 100, radians(-90), 0.1)  as f
-*/
-/*
-SELECT ST_AsText(ST_MakeLine(iline1, iline2) ) 
-FROM ST_GeomFromText('LINESTRING(0 0, 10 10)') as iline1 
-	, ST_GeomFromText('LINESTRING( 0 10,-10 0)') as iline2 
-*/
  
+  /*
+DROP SCHEMA IF EXISTS test ; 
 CREATE SCHEMA IF NOT EXISTS test ;
-/*
+
+
 DROP TABLE IF EXISTS test.making_parallelogram CASCADE; 
 CREATE TABLE test.making_parallelogram AS
 SELECT 1 AS gid, ipoint1::geometry(point,0), ipoint2::geometry(point,0) ,
 	iline::geometry(linestring,0), ibuff::geometry(polygon,0) 
 	, width ,alpha, support_line_size 
-	 
+	,rc_generate_parallelogram(
+		 iline 
+		, ipoint1  
+		,  ipoint2  
+		,  ibuff  
+		,  width  
+		, alpha
+		,  support_line_size  
+	)::geometry(polygon,0) as par
 FROM  ST_MakePoint(1 ,1)  AS ipoint1
 	, ST_MakePoint(2 ,1) AS ipoint2
 	, ST_GeomFromText('LINESTRING(-10 -10, 10 10)') as iline 
 	, ST_Buffer(iline, 4 ) as ibuff
 	, round(12.5::numeric,1) AS width
-	, round(pi()::numeric/2,1) AS alpha
+	, round(90::numeric,1) AS alpha
 	, round(1::numeric,1)  AS support_line_size
 	; 
-
-*/
-
-SELECT *
-FROM  test.making_parallelogram  ; 
-
-
-DROP TABLE IF EXISTS test.making_parallelogram_v CASCADE; 
-CREATE TABLE test.making_parallelogram_v AS
-SELECT DISTINCT ON (TRUE) 1 AS gid,  ST_SetSRID(rc_generate_parallelogram(
-		 iline 
-		, ipoint1  
-		,  ipoint2  
-		,  ST_Buffer(iline, 4 )   
-		,  width  
-		, radians(alpha)
-		,  support_line_size  
-	),4326)::geometry(polygon,4326) AS  f   
-FROM test.making_parallelogram AS mp  ;
 	
+	ALTER TABLE test.making_parallelogram ADD  PRIMARY KEY (gid) ; 
+
+
+
+	CREATE OR REPLACE FUNCTION test.rc_update_parallelogram()
+	RETURNS TRIGGER AS 
+	$BODY$ 
+	--- @brief :  udpate the geom of the parallelogram
+	 
+	DECLARE    
+	BEGIN   
+		NEW.ibuff := ST_Buffer(NEW.iline,  NEW.width/4.0 ); 
+		NEW.ipoint1  := ST_CLosestPoint(NEW.iline, NEW.ipoint1 ) ; 
+		NEW.ipoint2  := ST_CLosestPoint(NEW.iline, NEW.ipoint2 ) ; 
+		NEW.par :=  rc_lib.rc_generate_parallelogram(
+			 NEW.iline 
+			, NEW.ipoint1  
+			,  NEW.ipoint2  
+			,  NEW.ibuff  
+			,  NEW.width  
+			, NEW.alpha
+			,  NEW.support_line_size  
+		)::geometry(polygon,0) ; 
+		RETURN NEW; 
+	END ;
+	$BODY$  
+	 LANGUAGE plpgsql VOLATILE CALLED ON NULL INPUT;
+ 
+DROP TRIGGER IF EXISTS  rc_update_parallelogram ON   test.making_parallelogram ;
+CREATE TRIGGER rc_update_parallelogram 
+BEFORE UPDATE OR INSERT 
+ON  test.making_parallelogram 
+FOR ROW 
+EXECUTE PROCEDURE test.rc_update_parallelogram() ;
+
+ */
  
