1aef69136515bd13fc7f06192c2dcfa2366fd30d
Remi C <remi.fake@fake.com>
2015-09-08 12:09:42 +0200

    correcting rc_angle and creating a test file for it

--- postgis/rc_angle.sql
+++ postgis/rc_angle.sql
@@ -4,15 +4,17 @@
 -- This script gives an absolute angle between 3 points
 ------------- 
 
+--SET search_path to rc_lib, public;  
 
-DROP FUNCTION IF EXISTS rc_angle(  geom1 geometry, geom2 geometry, geom3 geometry);
+DROP FUNCTION IF EXISTS rc_angle(  geom1 geometry, geom2 geometry, geom3 geometry) ;
 CREATE OR REPLACE FUNCTION rc_angle(  geom1 geometry, geom2 geometry, geom3 geometry) RETURNS FLOAT AS
 $BODY$
 	--@brief : returns the oriented angle in radian  of the 3 geom centroids
 		DECLARE 
 			_temp float;  
-		BEGIN  
-			_temp := pi() + ST_Azimuth(ST_Centroid(geom1),ST_Centroid(geom2)) - ST_Azimuth(ST_Centroid(geom2),ST_Centroid(geom3)) ; 
+		BEGIN   
+			SELECT az + (az<0)::int*2*pi() INTO _temp
+			FROM (SELECT ST_Azimuth(ST_Centroid(geom1),ST_Centroid(geom2)) - ST_Azimuth(ST_Centroid(geom3),ST_Centroid(geom2)) AS az) AS sub; 
 			
 			RETURN _temp ; 
 		END ;
@@ -32,7 +34,4 @@ $BODY$
 		--should be 227.726310993906
 */
  
-
-
-SELECT ST_centroid(geom)
-FROM ST_GeomFromText('POINT(0 0)') AS geom  
+ 
---
+++ postgis/test/test_rc_angle.sql
@@ -0,0 +1,37 @@
+﻿---------------------------------------------
+--Rémi Cura , 2015
+----------------------------------------------
+-- This script test the way to compute angle between 3 points and use it to test left or right
+----------------------------------------------
+
+
+------------------
+
+-- testing the angle function
+
+--create a matrix of random points, so to test the angle between 3 points
+SET search_path TO test, rc_lib, public;
+
+
+DROP TABLE IF EXISTS test_angle_3_points ;
+CREATE TABLE  test_angle_3_points AS
+	WITH series AS (
+	    SELECT row_number() over() as gid,  s1+1/4 -random()/2.0 AS x , s2+1/4 -random()/2.0 AS y
+	    FROM generate_series(1,10) AS s1, generate_series(1,10) AS s2
+	)
+	, points AS (
+	    SELECT gid, ST_MakePoint(x+random()/2.0 , y+random()/2.0) AS p1
+		, ST_MakePoint(x+random()/2.0 , y+random()/2.0) AS p2
+		, ST_MakePoint(x+random()/2.0 , y+random()/2.0) AS p3
+	    FROM series AS s
+	)
+	SELECT gid, line, point , az_line, az_point, az_line-az_point AS diff_angle, cu_angle < 180 AS is_left , cu_angle
+		, (cu_angle <180 ) = (rc_angle>=180) as testing
+		, rc_angle
+		
+	FROM points
+	 , ST_MAkeline(p1,p2) AS line, ST_MAkeValid(p3) AS point
+	 , round(degrees(ST_Azimuth(p2,p1))::numeric,1) AS az_line, round(degrees(ST_Azimuth(point,p2))::numeric,1)AS az_point
+	 , round(CAST(  (az_line-az_point) + ((az_line-az_point)<0)::int*360 AS INT),3) AS cu_angle
+	, round(degrees(rc_lib.rc_angle(  p1,p2,p3)),3) AS rc_angle;
+-----------------------------------
