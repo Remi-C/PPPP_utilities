a7e2420826bb25c460755b8f3f19def6e4861250
Remi-C <remi.cura@gmail.com>
2016-01-22 20:17:54 +0100

    adding an option to only get on pass of the vehicule

--- pointcloud/lens_for_point.sql
+++ pointcloud/lens_for_point.sql
@@ -11,6 +11,8 @@ gid int  primary key CHECK (gid = 1)
 ,lens geometry(polygon,931008)
 ,max_nb_points int
 ,grid_cell_size float
+,tolerancy_on_time_range float
+,pass_number int
 )  ; 
 
 --inserting some default value in it
@@ -18,23 +20,52 @@ INSERT INTO tmob_20140616.lens_for_points VALUES
 (1
 ,  ST_GeomFromtext('POLYGON((650894.58 6861287.92, 650898.08 6861287.92, 650898.08 6861292.87, 650894.58 6861292.87,650894.58 6861287.92))',931008)
 ,10000	
-,0.05) 
+,0.05
+,60
+,1) 
 
 
 --creating a materialised view to extract point within the lense
 
+
 DROP MATERIALIZED VIEW IF EXISTS tmob_20140616.lens_points ; 
 CREATE MATERIALIZED VIEW tmob_20140616.lens_points AS 
- 
+
+	WITH patchs AS (
+		SELECT 	rp.gid, num_points, avg_time, geom
+		FROM tmob_20140616.lens_for_points AS lens 
+		, tmob_20140616.riegl_pcpatch_space_int_proxy AS rp
+		WHERE ST_Intersects(lens.lens,rp.geom) = TRUE
+	)
+	, u_ranges AS (
+		SELECT rc_numrange_disj_union(numrange((lower(avg_time)-tolerancy_on_time_range)::numeric,  (upper(avg_time)+tolerancy_on_time_range)::numeric) ORDER BY  avg_time ASC)  AS u_ranges
+		FROM tmob_20140616.lens_for_points, patchs
+	)
+	,u_range AS (
+		SELECT row_number() over(ORDER BY u_range ASC) AS u_id, u_range
+		FROM u_ranges, unnest(u_ranges) as u_range
+		ORDER BY u_range ASC
+	)
+	 ,selected_range AS (
+		SELECT u_range.*
+		FROM tmob_20140616.lens_for_points AS lens, u_range
+		WHERE lens.pass_number = u_id
+		LIMIT 1
+	)
 	SELECT (row_number() over())::int AS tid, pt::geometry(pointZ,931008) AS point  , PC_Get(point,'reflectance') as attributes
 	FROM tmob_20140616.lens_for_points AS lens 
-		, tmob_20140616.riegl_pcpatch_space_int_proxy AS rp
+		,selected_range
+		, patchs AS rp
 		 ,tmob_20140616.riegl_pcpatch_space_int AS rps 
 		 , rc_exploden_grid(patch, max_nb_points,grid_cell_size) as point
 		 , CAST(point AS geometry) AS pt
 	WHERE ST_Intersects(lens.lens,rp.geom) = TRUE
+		AND rp.avg_time && selected_range.u_range
 		AND rp.gid = rps.gid 
 			AND ST_Intersects(lens.lens,pt) = TRUE;
+
+ 
+
 
 
 -- creating trigger to automate refresh
