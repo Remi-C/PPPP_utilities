f6e1496f2bb037a98a22ef6e4f7eb1b027a99c6b
Remi-C <remi.cura@gmail.com>
2016-02-10 18:05:57 +0100

    working version, reworked to be clear, with benchmark and ploting

--- pointcloud/rc_ppl_octree.py
+++ pointcloud/rc_ppl_octree.py
@@ -11,53 +11,191 @@ using z ordering
 import numpy as np
 
 
-def test_ppl_octree():
-    npoints = [8,8,1]
+def benchmark_ppl():
+    import matplotlib.pyplot as plt
+    duration = []
+    square_size = []
+    min_square_size = 2
+    max_square_size = 64
+    for i in np.arange(min_square_size,max_square_size):
+        dur = test_ppl_octree(i)
+        duration.append(dur.total_seconds())
+        square_size.append(i)
+    
+    print duration 
+    print square_size
+    plt.plot(square_size,duration)
+    #plt.show()
+    
+     
+def test_ppl_octree(npo):
+    import datetime ; 
+    # creating test data 
+    npoints = [npo,npo,npo]
+    
     multi = 1
-    random_strength = 0.01
+    tot_level = 8
+    random_strength = 0. 
     points = []
     for i in np.arange(0,npoints[0]):
         for j  in np.arange(0,npoints[1]): 
             for k  in np.arange(0,npoints[2]): 
-                to_append=  np.round( ( np.array([i,j,k])  + (0.5 - np.random.random()) * random_strength ) * multi)
-                #to_append = np.array([i,j,k])
+                #to_append=  np.round( ( np.array([i,j,k])  + (0.5 - np.random.random()) * random_strength ) * multi)
+                to_append = np.array([i,j,k])
                 points.append(
                    to_append.astype(np.int)
-                    )   
+                    )  
+    points = np.vstack([points,points])
+    print points
     points = np.asarray(points)
     #plot_points(points)
-    #the points should be translated inreference to 0 
-    points = points- np.min(points)
-    return ppl_octree(points.astype(np.int32) )
+    print('snb of points : ', points.shape[0])
+    #points should be proprerly scaled, based on the expected precision,
+    #the biggest dimension between 0 and 2^N
+    time_start = datetime.datetime.now() 
+    ppl = pointcloud_to_ppl(points,tot_level)
+    time_end = datetime.datetime.now() 
+    duration = time_end-time_start
+    print 'duration : %s ' % (time_end-time_start)
+    print ppl
+    return duration
     
-def plot_points(points):
+    
+    #return ppl_octree(points.astype(np.int32) )
+    
+def plot_points(points, level=None):
     import numpy as np
     from mpl_toolkits.mplot3d import Axes3D
     import matplotlib.pyplot as plt
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d') 
-    ax.scatter(points[:,0], points[:,1], points[:,2] ) 
+    if level is None:
+        ax.scatter(points[:,0], points[:,1], points[:,2] ) 
+    else:
+        ax.scatter(points[:,0], points[:,1], points[:,2] ,linewidths=10/(level+1)) 
     plt.show()
 
+def plot_points_with_level(morton,level,enc):
+    """ given the morton code of points and a level per code and the encodor
+    print the points  with the code"""
+    
+    #decode morton
+    decoding = np.vectorize(enc.decode)
+    points = decoding( morton)
+    print points
+    print level
+    plot_points(np.asarray(points).T,level)
 
 # test : creating an array of random points
 # order by zcurve
 # count
 # return count 
+      
+def center_scale_quantize(pointcloud,tot_level):
+    """ Centers/scale the data so that everything is between 0 and 1
+        Don't deformate axis between theim. 
+        Quantize using the number of level 
+        @param a 2D numpy array, column = X,Y(,Z)  
+        @param the number of bit we want to quantize on  
+        @return a ne point cloud, same number of points, but everything between 0 and 1, and quantized
+        """    
+    #centering data so that all dim start at 0 
+    pointcloud_int = pointcloud - np.amin(pointcloud.T, axis=1); 
+     
+    
+    #finding the max scaling, that is the biggest range in dimension X or Y or Z
+    max_r = 1.0 ;
+    new_max = np.amax(np.amax(pointcloud_int.T,axis=1)) #look for max range in X then Y then Z, then take the max of it
 
-def affected_cell_number(point):
-    """ from the differnece of 2 morton codes, look for the highest bit and 
-    conclude about cell level"""
-    #import math 
-    return int(point).bit_length()
-    #return int(np.floor(math.log(diff_points, 2)))    
+    if new_max !=0: #protection against pointcloud with only one points or line(2D) or flat(3D)
+        max_r = new_max;
+    
+    #dividing so max scale is 0 . Now all the dimension are between [0,1]
+    pointcloud_int = pointcloud_int/ float(max_r) ; 
+    
+    #quantizing 
+    smallest_int_size_possible = max(8*np.ceil(tot_level/8.0)+1,8) #protection against 0 size
+    if smallest_int_size_possible > 8 : 
+        if smallest_int_size_possible > 32 :
+            smallest_int_size_possible = max(32*np.ceil(tot_level/32.0),32) #protection against 0 size
+        else :
+            smallest_int_size_possible = max(16*np.ceil(tot_level/16.0),16) #protection against 0 size
+     
+    pointcloud_int =  np.trunc(abs((pointcloud_int* pow(2,tot_level) )))\
+         .astype(np.dtype('uint'+str(int(smallest_int_size_possible))))
+    #plot_points(pointcloud_int)
+    #we have to take care of overflow : if we reach the max value of 1<<tot_level, we go one down
+    pointcloud_int[pointcloud_int==pow(2,tot_level)]=(pow(2,tot_level)-1);     
+    return pointcloud_int, smallest_int_size_possible
+ 
+def unique_rows(a): 
+    """ found on stack overflow"""
+    a = np.ascontiguousarray(a)
+    unique_a = np.unique(a.view([('', a.dtype)]*a.shape[1]))
+    return unique_a.view(a.dtype).reshape((unique_a.shape[0], a.shape[1]))    
+
+def pointcloud_to_ppl(untranslated_unscaled_points,tot_level):
+    """ given points, center scale quantize points, order pby morton
+    get number of points per level"""
+    #points need to be in [0,1]^3 * tot_level^2
+    #print untranslated_unscaled_points
+    points, coordinate_bit_size = center_scale_quantize(untranslated_unscaled_points,tot_level)
+    #plot_points(points)
+    
+    #removing duplicated points (would gives a wrong result and this is faster)
+    points = unique_rows(points)
+    
+    #converting coordinates to morton, order
+    morton_o, enc = order_array_by_morton(points, coordinate_bit_size) 
+    
+    #for each coordinate, apply XOR with next in morton order
+    diff = np.bitwise_xor(morton_o ,np.roll(morton_o,1,axis=0))
+    
+    #find the highest bit used (still interleaved, so warning)
+    h_bit_set = np.vectorize(highest_bit_set)
+    h_b_i = h_bit_set( diff)  
+    
+    #converting the interleaved highest bit to a non interleaved highest bit
+    ndim = points.shape[1] 
+    h_bit = np.floor((h_b_i +ndim-1 )/float(ndim))
+
+    #converting to level
+    level = -1 * ( h_bit-np.max(h_bit))
+    #plot_points_with_level(morton_o,level,enc)
+    
+    #counting point per level
+    ppl_iso = np.bincount(level.astype(int)) 
+    
+    #a point in level X should also be in level X + k
+    ppl = np.zeros(ppl_iso.shape[0]+1,dtype=np.int)
+    ppl[1:] = ppl_iso
+    for i in np.arange(2,ppl.shape[0]):
+        ppl[i] = ppl[i] + ppl[i-1]
+    ppl[0] = 1 
+    #returning result
+    return ppl 
+
+def highest_bit_set(point ):
+    """given an int, look for the highest bit set""" 
+    return int(point).bit_length()    
+    
+def order_array_by_morton(points, coordinate_bit_size):
+    import zorder
+    ndim = points.shape[1] 
+    #could be 32 or 64, we shalel chose
+    bits = 64
+    if coordinate_bit_size*3 <= 32:
+        bits = 32 
+    bits = 64 
+    enc = zorder.ZEncoder(ndim, bits)
+    morton = np.apply_along_axis(enc.encode, 1, points)   
+    return np.asarray(np.sort(morton)), enc
     
     
 def ppl_octree(points):
     import zorder
     #orde rpoints following z order
-    ndim = 3
-    
+    ndim = 3 
     enc = zorder.ZEncoder(ndim, bits = 64)
     morton = np.apply_along_axis(enc.encode, 1, points) 
     
@@ -77,7 +215,7 @@ def ppl_octree(points):
     decoded = decoding( diff)
     decoded =  np.max(decoded,axis = 0 )
     
-    finding_c_number = np.vectorize(affected_cell_number)
+    finding_c_number = np.vectorize(highest_bit_set)
     decoded_log = finding_c_number( decoded)
     level = -( decoded_log-np.max(decoded_log))
     #print decoded_log
@@ -91,7 +229,7 @@ def ppl_octree(points):
     
     
     for i in decoded:
-        print("\t \t",affected_cell_number(i))
+        print("\t \t",highest_bit_set(i))
         print np.binary_repr(i, width = 8)
         
     print decoded
@@ -101,7 +239,7 @@ def ppl_octree(points):
     for i in diff:
         #print np.binary_repr(i,width=32)
         max_level = np.max(enc.decode(i)) 
-        print("\t \t",affected_cell_number(max_level))
+        print("\t \t",highest_bit_set(max_level))
         print np.binary_repr(max_level, width = 8)
         
     
@@ -111,7 +249,7 @@ def ppl_octree(points):
     print np.bincount(level)
     return
       
-    finding_c_number = np.vectorize(affected_cell_number)
+    finding_c_number = np.vectorize(highest_bit_set)
     lev_offset = finding_c_number( diff)
     print('lev_offset')
     print lev_offset
@@ -135,4 +273,5 @@ def ppl_octree(points):
            
     return ppl_octree
 
-test_ppl_octree()
+#test_ppl_octree(10)
+#benchmark_ppl()
