711b50183cc5ea628935a0b08dda037e894eb309
Remi-C <remi.cura@gmail.com>
2016-02-10 18:09:53 +0100

    example of file to perform out of the database fast_ppl computing

---
+++ pointcloud/fast_ppl_out_of_server.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 15:53:24 2016
+
+@author: RCura
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+import psycopg2
+
+def connect_to_base():
+    conn = psycopg2.connect(  
+        database='test_pointcloud'
+        ,user='postgres'
+        ,password='postgres'
+        ,host='172.16.3.50'
+        ,port='5432' ) 
+    cur = conn.cursor()
+    return conn, cur  
+
+def execute_querry(q,arg_list,conn,cur):  
+    #print q % arg_list    
+    cur.execute( q ,arg_list)
+    conn.commit()
+
+#connect to database, get a list of lines to work on
+#disconnect
+#divide the list into chuncks
+
+#treat a chunck with a connection, push results, close connection
+
+def get_list_of_job(limit):
+    import numpy as np
+    #connect to database
+    conn, cur = connect_to_base()
+    #get thelist of job
+    q = """ SELECT gid 
+        FROM lod.dim_descr_comparison
+        WHERE points_per_level IS NOT NULL
+        --AND gid = 906820
+        ORDER BY gid ASC 
+        LIMIT %s """ 
+    execute_querry(q,[limit],conn,cur)
+    gid = cur.fetchall() 
+    gid = np.asarray(gid).T[0]
+    cur.close()
+    conn.close()
+    return gid
+
+
+def cut_list_into_chunk(gid, max_chunk_size):
+    """ given a list , tries to cut it into smaller parts of max_chunk_size """
+    import numpy as np
+    import math
+    result = []
+    for i in np.arange(0,math.floor(gid.size/max_chunk_size)+1):
+        key_local_start = int(math.ceil(i * max_chunk_size)) 
+        key_local_end = int(math.trunc((i+1) * max_chunk_size))
+        key_local_end = gid.size if key_local_end > gid.size else key_local_end
+        extract = gid[np.arange(key_local_start,key_local_end)]
+        if extract.size >0 :
+            result.append(gid[np.arange(key_local_start,key_local_end)])
+                
+    return result
+
+#import numpy as np
+#gid = np.arange(1,100)
+#cut_list_into_chunk(gid, 3)
+
+
+def process_one_chunk( gid_extract  ):
+    """ given a subset of the gid, do something with it"""    
+    connection_string = "dbname=test_pointcloud user=postgres password=postgres host=172.16.3.50 port=5432"
+    
+    #create connection
+    conn, cur = connect_to_base()
+    #deal with the chunk 
+    for i in gid_extract:
+        #print('i ',i) 
+        try: 
+            result = process_one_gid(i,conn, cur,connection_string) 
+        except: 
+            err = 'error with gid : '+str(i)
+            raise# NameError(err)
+    #close connection
+    cur.close()
+    conn.close() 
+        
+        
+
+
+def process_one_gid(one_gid, conn, cur, connection_string):
+    """given one gid, process """
+    import pg_pointcloud_classes as pgp
+    import rc_ppl_octree as ppl
+    import numpy as np
+    #get data
+    
+    q = """ SELECT pc_uncompress(patch),6 
+        FROM acquisition_tmob_012013.riegl_pcpatch_space_proxy 
+        WHERE gid = %s   """
+    arg_list = [one_gid.tolist()]
+    execute_querry(q,arg_list,conn,cur)
+    result =  cur.fetchall()[0]
+    u_patch = result[0] 
+    tot_level = result[1]
+    
+    #process  
+    GD = pgp.create_GD_if_not_exists()
+    #cache mecanism for patch schema
+    if 'rc' not in GD:  # creating the rc dict if necessary
+        GD['rc'] = dict()
+    if 'schemas' not in GD['rc']:  # creating the schemas dict if necessary
+    	GD['rc']['schemas'] = dict() 
+    
+    restrict_dim = ["x","y","z"] 
+    points_double, mschema = pgp.WKB_patch_to_numpy_double(u_patch,
+                                                           GD['rc']['schemas'],
+                                                           connection_string,
+                                                           restrict_dim)
+
+    #processnig : 
+    ppl_octree = ppl.pointcloud_to_ppl(points_double,tot_level) 
+    
+    
+    
+     
+    #write result in base
+    q = """ UPDATE lod.dim_descr_comparison
+            SET ( points_per_level_py ) = (%s  )
+        WHERE gid = %s; """
+    arg_list = [ppl_octree.tolist(),one_gid]
+    #printing_arglist(arg_list)
+    execute_querry(q,arg_list,conn,cur) 
+    return True
+
+def printing_arglist( arg_list):
+    print('gid of the patch')
+    print(arg_list[1])
+    print('fast_ppl')
+    print(arg_list[0]) 
+    
+    
+def test_mono():
+    import numpy as np
+    max_chunk_size = 4
+    overall_max = 4
+    
+    gid = get_list_of_job(overall_max)
+    #print("gid : ",gid)
+    gid_sequenced = cut_list_into_chunk(np.asarray(gid), max_chunk_size)
+    #print('gid_sequenced ',gid_sequenced)
+    for i in gid_sequenced:
+        process_one_chunk(i)
+
+
+
+def multiprocess():
+    import  multiprocessing as mp; 
+    import random;  
+    import numpy as np
+    import datetime ; 
+    
+    time_start = datetime.datetime.now(); 
+    print 'starting : %s ' % (time_start); 
+    
+    processes = 8
+    max_chunk_size = 10
+    overall_max = 25000
+    
+    gid = get_list_of_job(overall_max)
+    
+    #print("gid : ",gid)
+    gid_sequenced = cut_list_into_chunk(np.asarray(gid), max_chunk_size) 
+    random.shuffle(gid_sequenced)
+    print 'job in line, ready to process : %s ' % (datetime.datetime.now()); 
+    #print('gid_sequenced ',gid_sequenced) 
+    pool = mp.Pool(processes)
+    results = pool.map(process_one_chunk, gid_sequenced)
+    time_end = datetime.datetime.now(); 
+    print 'ending : %s ' % (time_end); 
+    print 'duration : %s ' % (time_end-time_start)
+    return results
+    
+#test_mono()
+
+
+##dirty windows trick
+def main():
+    multiprocess()
+if __name__ == "__main__":
+    main()
