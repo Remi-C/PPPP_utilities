a7736a17400859f585bf50d8c23abf9e26a83bae
Remi-C <remi.cura@gmail.com>
2016-02-15 10:20:43 +0100

    removing re-declaration of a postgres function

--- pointcloud/patch_to_raster/patch_to_raster_all_pixels.sql
+++ pointcloud/patch_to_raster/patch_to_raster_all_pixels.sql
@@ -346,23 +346,7 @@
 	$BODY$
 	 LANGUAGE plpgsql VOLATILE;
 
-
-
-	DROP FUNCTION IF EXISTS rc_round(in val anyelement , IN round_step anyelement, out o_val DOUBLE PRECISION) ;
-	CREATE OR REPLACE FUNCTION rc_round(in val anyelement , IN round_step anyelement, out o_val double precision) AS
-	$BODY$
-			--@brief : this function round the input value to the nearest value multiple of round_step
-		DECLARE			 
-		BEGIN 
-
-		o_val := (round(val::numeric/round_step::numeric)*round_step::numeric)::double precision;
-		RETURN ;
-		END;
-	$BODY$
-	 LANGUAGE plpgsql  IMMUTABLE STRICT;
-
-	--SELECT rc_round(235.2499, 0.02)
-
+  
 	--select pg_backend_pid();
 
 
