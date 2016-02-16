e2cdad54f28597ccda67358edac815777f6ca29c
Remi-C <***@***.com>
2016-02-16 18:22:14 +0100

    new sql wrapper for octree occupancy and dim descriptor computing

---
+++ pointcloud/rc_dimensionality_feature.sql
@@ -0,0 +1,29 @@
+﻿--------------------------
+-- Remi CUra, thales IGN 2016
+--
+-- Dimensionality feature through inbase processing (wrapper for python functions)
+--------------------------
+
+
+DROP FUNCTION IF EXISTS rc_ppl_to_dim_feature (int[], int );
+CREATE OR REPLACE FUNCTION rc_ppl_to_dim_feature (IN ppl int[], IN num_points int,out median_dim float,OUT ransac_dim float,OUT ransac_confidence float,OUT dim_lod float[],OUT dim_loddiff float[])
+AS $$ 
+import numpy as np
+import dimensionality_feature as dim 
+
+ppl2 = np.asarray(ppl).astype(np.float64)
+
+multiscale_dim, multiscale_dim_var, multiscale_fused, theoretical_dim, cov = dim.compute_rough_descriptor(ppl2,num_points)
+ 
+median_dim = multiscale_fused
+ransac_dim = theoretical_dim
+ransac_confidence= cov
+dim_lod = np.round(multiscale_dim,3).tolist()
+dim_loddiff = np.round(multiscale_dim_var,3).tolist()
+
+return (median_dim,ransac_dim ,ransac_confidence, dim_lod , dim_loddiff) 
+$$ LANGUAGE plpythonu;
+
+
+-- SELECT *
+-- FROM rc_ppl_to_dim_feature(ARRAY[1,4,16,62],83) ; 
---
+++ pointcloud/rc_octree_occupancy.sql
@@ -0,0 +1,63 @@
+﻿--------------------------
+-- Remi CUra, thales IGN 2016
+--
+-- octree occupancy through python module (wrapper)
+--------------------------
+
+
+DROP FUNCTION IF EXISTS rc_multipoints_to_ppl (geometry, int );
+CREATE OR REPLACE FUNCTION rc_multipoints_to_ppl (ipoints geometry , tot_level int,OUT ppl int[])
+AS $$ 
+import numpy as np 
+import rc_ppl_octree as rcppl
+import shapely as sh 
+#converting points to appropriate numpy array
+
+#importing the shapely package to perform geometry manipulation
+from shapely import wkb ; #loading geometry from postgres 
+ 
+#importing the geometry 
+points = np.asarray(wkb.loads( ipoints, hex=True) )   
+#using external module
+ppl = rcppl.pointcloud_to_ppl(points,tot_level)
+
+return (ppl.tolist()) 
+$$ LANGUAGE plpythonu;
+
+/*
+SELECT f.*
+FROM CAST(8 AS int) AS tot_level
+	, ST_GeomFromText('MULTIPOINTZ( 0 0 0, 1 0 0 , 2 0 0 , 0 1 0, 1 1 0 , 2 1 0  ,0 2 0, 1 2 0 , 2 2 0  )') AS ipoints
+	,rc_multipoints_to_ppl(ipoints, tot_level) as f ;
+*/
+
+DROP FUNCTION IF EXISTS rc_patch_to_ppl (pcpatch, int );
+CREATE OR REPLACE FUNCTION rc_patch_to_ppl (ipatch pcpatch , tot_level int,OUT ppl int[])
+AS $$ 
+import numpy as np 
+import rc_ppl_octree as rcppl   
+import pg_pointcloud_classes as pgp
+
+#convert patch to numpy array 
+GD = pgp.create_GD_if_not_exists()
+#cache mecanism for patch schema
+if 'rc' not in GD:  # creating the rc dict if necessary
+	GD['rc'] = dict()
+	if 'schemas' not in GD['rc']:  # creating the schemas dict if necessary
+		GD['rc']['schemas'] = dict() 
+
+restrict_dim = ["x","y","z"]
+#converting patch to appropriate numpy array
+points_double, mschema = pgp.WKB_patch_to_numpy_double(ipatch, GD['rc']['schemas'], None, dim_to_use=restrict_dim)
+   
+#using external module
+ppl = rcppl.pointcloud_to_ppl(points_double,tot_level)
+
+return (ppl.tolist()) 
+$$ LANGUAGE plpythonu;
+
+/*
+SELECT  f.*
+ FROM lod.bench_small_cubes ,  rc_patch_to_ppl( pc_uncompress(patch), 8) AS f 
+ WHERE gid = 7359;  
+ */
