1402ffac5f434916f4af315c87a7de322e35d1df
Rémi-C <remi.cura@gmail.com>
2016-02-23 10:06:39 +0100

    new function to comput dimensionlity from patch

--- pointcloud/rc_dimensionality_feature.sql
+++ pointcloud/rc_dimensionality_feature.sql
@@ -27,3 +27,29 @@ $$ LANGUAGE plpythonu;
 
 -- SELECT *
 -- FROM rc_ppl_to_dim_feature(ARRAY[1,4,16,62],83) ; 
+
+DROP FUNCTION IF EXISTS rc_patch_to_dim_cov (PCPATCH );
+CREATE OR REPLACE FUNCTION rc_patch_to_dim_cov (ipatch PCPATCH, OUT dim_cov FLOAT)
+AS $$ 
+import numpy as np
+import dimensionality_feature as dim 
+import pg_pointcloud_classes as pgp
+
+#convert patch to numpy array 
+GD = pgp.create_GD_if_not_exists()
+#cache mecanism for patch schema
+if 'rc' not in GD:  # creating the rc dict if necessary
+	GD['rc'] = dict()
+	if 'schemas' not in GD['rc']:  # creating the schemas dict if necessary
+		GD['rc']['schemas'] = dict() 
+
+restrict_dim = ["x","y","z"]
+#converting patch to appropriate numpy array
+points_double, mschema = pgp.WKB_patch_to_numpy_double(ipatch, GD['rc']['schemas'], None, dim_to_use=restrict_dim)
+
+p_dim = dim.compute_descriptors_from_points(points_double)
+dim_cov = dim.proba_to_dim_power(p_dim)
+ 
+return (dim_cov) 
+$$ LANGUAGE plpythonu;
+
