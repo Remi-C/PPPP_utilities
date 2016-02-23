c88cc77e6b30616f398961fde910437c9c4381bc
Rémi-C <remi.cura@gmail.com>
2016-02-23 10:07:05 +0100

    correcting and new function from xyz array, for performance

--- pointcloud/rc_octree_occupancy.sql
+++ pointcloud/rc_octree_occupancy.sql
@@ -6,18 +6,32 @@
 
 
 DROP FUNCTION IF EXISTS rc_multipoints_to_ppl (geometry, int );
-CREATE OR REPLACE FUNCTION rc_multipoints_to_ppl (ipoints geometry , tot_level int,OUT ppl int[])
+CREATE OR REPLACE FUNCTION rc_lib.rc_multipoints_to_ppl (ipoints geometry , tot_level int,OUT ppl int[])
 AS $$ 
 import numpy as np 
-import rc_ppl_octree as rcppl
-import shapely as sh 
+import rc_ppl_octree as rcppl 
 #converting points to appropriate numpy array
 
 #importing the shapely package to perform geometry manipulation
-from shapely import wkb ; #loading geometry from postgres 
- 
+from shapely import wkb ; #loading geometry from postgres  
 #importing the geometry 
-points = np.asarray(wkb.loads( ipoints, hex=True) )   
+points = np.asarray(wkb.loads( ipoints, hex=True) )    
+#using external module
+ppl = rcppl.pointcloud_to_ppl(points,tot_level)
+
+return (ppl.tolist()) 
+$$ LANGUAGE plpythonu;
+
+
+DROP FUNCTION IF EXISTS rc_xyz_array_to_ppl (float[] , int );
+CREATE OR REPLACE FUNCTION rc_xyz_array_to_ppl (coordinates float[] , tot_level int,OUT ppl int[])
+AS $$
+""" expects an array of X1 Y1, Z1, X2, Y2, Z2, X3 ..."""
+import numpy as np 
+import rc_ppl_octree as rcppl 
+#converting points to appropriate numpy array
+points = np.asarray(coordinates).reshape([-1,3]) 
+ 
 #using external module
 ppl = rcppl.pointcloud_to_ppl(points,tot_level)
 
@@ -27,8 +41,9 @@ $$ LANGUAGE plpythonu;
 /*
 SELECT f.*
 FROM CAST(8 AS int) AS tot_level
-	, ST_GeomFromText('MULTIPOINTZ( 0 0 0, 1 0 0 , 2 0 0 , 0 1 0, 1 1 0 , 2 1 0  ,0 2 0, 1 2 0 , 2 2 0  )') AS ipoints
-	,rc_multipoints_to_ppl(ipoints, tot_level) as f ;
+	, ST_GeomFromText('MULTIPOINTZ( 0 0 1, 1 0 0 , 2 0 0 , 0 1 0, 1 1 0 , 2 1 0  ,0 2 0, 1 2 0 , 2 2 1  )') AS ipoints
+	,  rc_lib.rc_multipoints_to_XYZ_array(ipoints, rounding_digits:=3) as arr_coordinates
+	,rc_lib.rc_xyz_array_to_ppl(arr_coordinates, tot_level) as f ;
 */
 
 DROP FUNCTION IF EXISTS rc_patch_to_ppl (pcpatch, int );
