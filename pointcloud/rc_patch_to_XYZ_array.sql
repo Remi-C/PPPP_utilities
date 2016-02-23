bc529ed369b2587338be5520d868e63c9ca03664
Rémi-C <remi.cura@gmail.com>
2016-02-23 10:07:35 +0100

    new multipoint ot XYZ array, for performance

--- pointcloud/rc_patch_to_XYZ_array.sql
+++ pointcloud/rc_patch_to_XYZ_array.sql
@@ -18,9 +18,7 @@
 	CREATE OR REPLACE FUNCTION rc_patch_to_XYZ_array(ipatch PCPATCH,maxpoints INT DEFAULT 0, rounding_digits int default 3
 		)
 	  RETURNS FLOAT[] AS
-	$BODY$
-			--@brief this function clean result tables
-			-- @return :  nothing 
+	$BODY$ 
 			DECLARE 
 			BEGIN 
 				RETURN array_agg_custom(
@@ -34,4 +32,35 @@
 		$BODY$
 	LANGUAGE plpgsql IMMUTABLE STRICT;
 	--SELECT rc_patch_to_XYZ_array()
+
+	DROP FUNCTION IF EXISTS rc_lib.rc_multipoints_to_XYZ_array(ipoints geometry, int);
+	CREATE OR REPLACE FUNCTION rc_lib.rc_multipoints_to_XYZ_array(ipoints geometry,  rounding_digits int default 3 )
+	  RETURNS FLOAT[] AS
+	$BODY$ 
+			DECLARE 
+			BEGIN 
+				IF ST_Zmflag(ipoints) =2 THEN 
+				RETURN rc_lib.array_agg_custom(
+					ARRAY[
+						 round(ST_X(dmp.geom)::numeric,rounding_digits)
+						,  round(ST_Y(dmp.geom)::numeric,rounding_digits)
+						,  round(ST_Z(dmp.geom)::numeric,rounding_digits)
+					] ORDER BY dmp.path ASC )
+				FROM ST_DumpPoints(ipoints ) AS dmp  ; 
+			ELSIF ST_Zmflag(ipoints) =0 THEN
+				RETURN rc_lib.array_agg_custom(
+					ARRAY[
+						 round(ST_X(dmp.geom)::numeric,rounding_digits)
+						,  round(ST_Y(dmp.geom)::numeric,rounding_digits) 
+					] ORDER BY dmp.path ASC )
+				FROM ST_DumpPoints(ipoints ) AS dmp  ; 
+			ELSE  
+				RAISE EXCEPTION 'mutlipoints to array not supported for type %' , ST_Zmflag(ipoints)  ; 
+			END IF ; 
+			END ; 
+		$BODY$
+	LANGUAGE plpgsql IMMUTABLE STRICT;
+	--SELECT rc_patch_to_XYZ_array()
+		SELECT rc_lib.rc_multipoints_to_XYZ_array(ipoints,3) 
+		FROM ST_GeomFromText('MULTIPOINT( 0 0 0,1 1 1,2 2 2,3 3 3 ,4 4 4)') as ipoints;
  
